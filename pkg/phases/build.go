548cc0702603e8193e6797d489c83b12da394ce6
Moshe Immerman <moshe.immerman@gmail.com>
2019-09-02 10:42:40 +0200

    support symlinked manifests

--- pkg/phases/build.go
+++ pkg/phases/build.go
@@ -1,17 +1,16 @@
 package phases
 
 import (
+	"github.com/moshloop/platform-cli/pkg/types"
+	"github.com/moshloop/platform-cli/pkg/utils"
 	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"os"
 	"path"
-
-	"github.com/moshloop/platform-cli/pkg/types"
-	"github.com/moshloop/platform-cli/pkg/utils"
 )
 
 var manifests = []string{
-	"https://docs.projectcalico.org/v3.8/manifests/calico.yaml",
 	"https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.9/deploy/local-path-storage.yaml",
 	"https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml",
 	"https://raw.githubusercontent.com/heptiolabs/eventrouter/master/yaml/eventrouter.yaml",
@@ -22,7 +21,7 @@ func Build(cfg types.PlatformConfig) error {
 	tmp, _ := ioutil.TempFile("", "config*.yml")
 	data, _ := yaml.Marshal(cfg)
 	tmp.WriteString(string(data))
-	os.Mkdir("build", 0750)
+	os.Mkdir("build", 0755)
 	for _, manifest := range manifests {
 		name := path.Base(manifest)
 		dest := "build/" + name
@@ -39,7 +38,8 @@ func Build(cfg types.PlatformConfig) error {
 
 	for _, spec := range cfg.Specs {
 		log.Infof("Building specs in %s", spec)
-		if err := utils.Exec(".bin/gomplate --input-dir \"%s\" --output-dir build -c \".=%s\"", spec, tmp.Name()); err != nil {
+		absPath, _ := os.Readlink(spec)
+		if err := utils.Exec(".bin/gomplate --input-dir \"%s\" --output-dir build -c \".=%s\"", absPath, tmp.Name()); err != nil {
 			return err
 		}
 	}
