8b11acd150cc8fbfb5ba56f8f516adc03ab17b96
Moshe Immerman <moshe.immerman@gmail.com>
2019-10-25 10:31:03 +0200

    build fix

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-alias go="docker run --rm -it -v $PWD:$PWD -w $PWD golang:1.13.1"
-go version
-
+BIN=./.bin/platform-cli
+PLATFORM_CONFIG=test/common.yml
 if [[ ! -e ./kind ]]; then
   curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-$(uname)-amd64
   chmod +x ./kind
@@ -11,15 +10,14 @@ if ! which gojsontoyaml 2>&1 ; then
   go get -u github.com/brancz/gojsontoyaml
 fi
 
+docker run --rm -it -v $PWD:$PWD -v /go:/go -w $PWD --entrypoint make golang:1.12 setup pack
+
 kubernetes_version=$(cat test/common.yml | gojsontoyaml -yamltojson | jq -r '.kubernetes.version')
 ./kind create cluster --image kindest/node:${kubernetes_version} --config test/kind.config.yaml
 KUBECONFIG="$(./kind get kubeconfig-path --name="kind")"
-go env
-make setup
-make pack
-PLATFORM_CONFIG=test/common.yml
-platform-cli version
-platform-cli deploy base  -vvv
-platform-cli deploy stubs  -vvv
-platform-cli deploy all  -vvv
-platform-cli test all -vvv
+
+$BIN version
+$BIN deploy base  -vvv
+$BIN deploy stubs  -vvv
+$BIN deploy all  -vvv
+$BIN test all -vvv
