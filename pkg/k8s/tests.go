3efa37d73ec2870519c1016cd0bf447285680fd7
Moshe Immerman <moshe.immerman@gmail.com>
2019-10-25 14:42:30 +0200

    add events to failed k8s pods

--- pkg/k8s/tests.go
+++ pkg/k8s/tests.go
@@ -1,6 +1,8 @@
 package k8s
 
 import (
+	"fmt"
+
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -9,7 +11,9 @@ import (
 )
 
 func TestNamespace(client kubernetes.Interface, ns string, t *console.TestResults) {
-	list, err := client.CoreV1().Pods(ns).List(metav1.ListOptions{})
+	pods := client.CoreV1().Pods(ns)
+	events := client.CoreV1().Events(ns)
+	list, err := pods.List(metav1.ListOptions{})
 	if err != nil {
 		t.Failf("Failed to get pods for %s: %v", ns, err)
 		return
@@ -19,7 +23,21 @@ func TestNamespace(client kubernetes.Interface, ns string, t *console.TestResult
 		if pod.Status.Phase == v1.PodRunning || pod.Status.Phase == v1.PodSucceeded {
 			t.Passf(ns, "%s => %s", pod.Name, pod.Status.Phase)
 		} else {
-			t.Failf(ns, "%s => %s ", pod.Name, pod.Status.Phase)
+			events, err := events.List(metav1.ListOptions{
+				FieldSelector: "involvedObject.name=" + pod.Name,
+			})
+			if err != nil {
+				t.Failf(ns, "%s => %s, failed to get events %+v ", pod.Name, pod.Status.Phase, err)
+				continue
+			}
+			msg := ""
+			for _, event := range events.Items {
+				if event.Type == "Normal" {
+					continue
+				}
+				msg += fmt.Sprintf("%s: %s ", event.Reason, event.Message)
+			}
+			t.Failf(ns, "%s=%s %s ", pod.Name, pod.Status.Phase, msg)
 		}
 	}
 	// check all pods running or completed with < 3 restarts
