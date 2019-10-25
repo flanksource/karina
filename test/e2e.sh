3f7fb62aed1b2458501b4c07e0015691e60ac1c8
Moshe Immerman <moshe.immerman@gmail.com>
2019-10-25 09:57:13 +0200

    first draft of e2e tests

---
+++ test/e2e.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ ! -e ./kind ]]; then
+  curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-$(uname)-amd64
+  chmod +x ./kind
+fi
+
+if ! which gojsontoyaml 2>&1 ; then
+  go get -u github.com/brancz/gojsontoyaml
+fi
+
+
+kubernetes_version=$(cat test/common.yml | gojsontoyaml -yamltojson | jq -r '.kubernetes.version')
+./kind create cluster --image kindest/node:${kubernetes_version} --config test/kind.config.yaml
+
+make setup
+make pack
+PLATFORM_CONFIG=test/common.yml
+platform-cli version
+platform-cli deploy base  -vvv
+platform-cli deploy stubs  -vvv
+platform-cli deploy all  -vvv
+platform-cli test all -vvv
