00bbf8b6933cef6bab0f02b1dcdd3a6e85d4e323
Moshe Immerman <moshe.immerman@gmail.com>
2019-10-25 11:55:53 +0200

    add expenv and wait for base test to pass

--- test/e2e.sh
+++ test/e2e.sh
@@ -7,8 +7,14 @@ if [[ ! -e ./kind ]]; then
   chmod +x ./kind
 fi
 
-if ! which gojsontoyaml 2>&1 ; then
+if ! which gojsontoyaml 2>&1 /dev/null; then
   go get -u github.com/brancz/gojsontoyaml
+fi
+
+if ! which expenv 2>&1 > /dev/null; then
+  wget https://github.com/CrunchyData/postgres-operator/releases/download/v4.1.0/expenv
+  chmod +x expenv
+  mv expenv /usr/local/bin
 fi
 
 docker run --rm -it -v $PWD:$PWD -v /go:/go -w $PWD --entrypoint make golang:1.12 setup pack
@@ -16,9 +22,19 @@ docker run --rm -it -v $PWD:$PWD -v /go:/go -w $PWD --entrypoint make golang:1.1
 kubernetes_version=$(cat test/common.yml | gojsontoyaml -yamltojson | jq -r '.kubernetes.version')
 ./kind create cluster --image kindest/node:${kubernetes_version} --config test/kind.config.yaml
 export KUBECONFIG="$(./kind get kubeconfig-path --name="kind")"
-M
 $BIN version
 $BIN deploy base -vvv
+$BIN deploy calico -vvv
 $BIN deploy stubs -vvv
+
+
+# wait for base to be up for up to +- 50 seconds
+for i in {1..10}; do
+ if $BIN test base; then
+    break
+  fi
+  sleep 5
+done
+
 $BIN deploy all -vvv
 $BIN test all -vvv
