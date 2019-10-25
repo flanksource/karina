73862b4102eff58cdffb0677fb1ca6e29b432894
Moshe Immerman <moshe.immerman@gmail.com>
2019-10-25 10:50:00 +0200

    test fix

--- test/e2e.sh
+++ test/e2e.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BIN=./.bin/platform-cli
-PLATFORM_CONFIG=test/common.yml
+export PLATFORM_CONFIG=test/common.yml
 if [[ ! -e ./kind ]]; then
   curl -Lo ./kind https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-$(uname)-amd64
   chmod +x ./kind
@@ -14,10 +14,10 @@ docker run --rm -it -v $PWD:$PWD -v /go:/go -w $PWD --entrypoint make golang:1.1
 
 kubernetes_version=$(cat test/common.yml | gojsontoyaml -yamltojson | jq -r '.kubernetes.version')
 ./kind create cluster --image kindest/node:${kubernetes_version} --config test/kind.config.yaml
-KUBECONFIG="$(./kind get kubeconfig-path --name="kind")"
+export KUBECONFIG="$(./kind get kubeconfig-path --name="kind")"
 
 $BIN version
-$BIN deploy base -c $PLATFORM_CONFIG -vvv
-$BIN deploy stubs -c $PLATFORM_CONFIG -vvv
-$BIN deploy all -c $PLATFORM_CONFIG -vvv
-$BIN test all -c $PLATFORM_CONFIG -vvv
+$BIN deploy base -vvv
+$BIN deploy stubs -vvv
+$BIN deploy all -vvv
+$BIN test all -vvv
