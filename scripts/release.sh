ce4acd529e65425c11793e645f232b60d8006d21
Moshe Immerman <moshe.immerman@gmail.com>
2019-11-08 11:54:34 +0200

    fix binary packaging

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,23 +8,10 @@ if [[ "$TAG" == "" ]];  then
   exit 0
 fi
 
-
-if ! which goreleaser 2>&1 > /dev/null; then
-  # need to pin the version
-  wget -nv https://github.com/goreleaser/goreleaser/releases/download/v0.108.0/goreleaser_amd64.deb
-  sudo dpkg -i goreleaser_amd64.deb
-fi
-
-if ! which rpmbuild 2>&1 > /dev/null; then
-  sudo apt-get update && sudo apt-get install -y rpm
-fi
-
-git stash
-git clean -fd
-goreleaser --rm-dist --skip-validate
+make setup linux darwin
 
 GO111MODULE=off go get github.com/aktau/github-release
 go get github.com/aktau/github-release
-github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME} -f dist/linux_amd64/${NAME}
-github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}_osx -f dist/darwin_amd64/${NAME}
-github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}.exe -f dist/windows_amd64/${NAME}.exe
+github-release release -u $GITHUB_USER -r ${NAME} --tag $TAG
+github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME} -f .bin/${NAME}
+github-release upload -u $GITHUB_USER -r ${NAME} --tag $TAG -n ${NAME}_osx -f .bin/${NAME}_osx
