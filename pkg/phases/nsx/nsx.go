359ed24f72280eea9acb2c30a0127275a4350654
Moshe Immerman <moshe.immerman@gmail.com>
2020-01-01 19:46:09 +0200

    remove unused files / packages

--- pkg/phases/nsx/nsx.go
+++ pkg/phases/nsx/nsx.go
@@ -7,13 +7,67 @@ import (
 	"github.com/fatih/structs"
 	log "github.com/sirupsen/logrus"
 
+	"github.com/moshloop/commons/certs"
+	"github.com/moshloop/commons/console"
 	"github.com/moshloop/platform-cli/pkg/platform"
-	"github.com/moshloop/platform-cli/pkg/utils"
 )
 
 const (
 	Namespace = "nsx-system"
+	CertName  = "nsx-secret"
 )
+
+func Install(p *platform.Platform) error {
+	if p.NSX == nil || p.NSX.Disabled {
+		return nil
+	}
+
+	if err := p.CreateOrUpdateNamespace(Namespace, nil, nil); err != nil {
+		return err
+	}
+	p.NSX.Image = p.GetImagePath("library/nsx-ncp-ubuntu:" + p.NSX.Version)
+	if err := p.ApplySpecs(Namespace, "nsx.yaml"); err != nil {
+		return err
+	}
+
+	if !p.HasSecret(Namespace, CertName) {
+		cert := certs.NewCertificateBuilder("kubernetes-client").Certificate
+		cert, err := p.CA.SignCertificate(cert, 10)
+		if err != nil {
+			return err
+		}
+
+		if err := p.CreateOrUpdateSecret(CertName, Namespace, cert.AsTLSSecret()); err != nil {
+			return err
+		}
+	}
+
+	// p.NSX.NsxV3.NsxApiCertFile = "/etc/nsx-ujo/nsx-cert/tls.crt"
+	// p.NSX.NsxV3.NsxApiPrivateKeyFile = "/etc/nsx-ujo/nsx-cert/tls.key"
+	yes := true
+	p.NSX.NsxV3.Insecure = &yes
+	p.NSX.NsxCOE.Cluster = p.Name
+
+	ini := structs.Map(p.NSX)
+
+	s := "[Defaults]\n" + mapToINI(ini)
+
+	log.Tracef("Using NSX config: %s", console.StripSecrets(string(s)))
+
+	if err := p.CreateOrUpdateConfigMap("nsx-ncp-config", Namespace, map[string]string{
+		"ncp.ini": string(s),
+	}); err != nil {
+		return err
+	}
+
+	if err := p.CreateOrUpdateConfigMap("nsx-node-agent-config", Namespace, map[string]string{
+		"ncp.ini": string(s),
+	}); err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func mapToINI(ini map[string]interface{}) string {
 	s := ""
@@ -44,50 +98,4 @@ func mapToINI(ini map[string]interface{}) string {
 		}
 	}
 	return s
-}
-
-func Install(p *platform.Platform) error {
-	p.NSX.Image = p.GetImagePath("library/nsx-ncp-ubuntu:" + p.NSX.Version)
-	if err := p.ApplySpecs(Namespace, "nsx.yaml"); err != nil {
-		return err
-	}
-
-	cert := p.Certificates.Root.ToCert()
-	log.Infof("Creating NSX cert for %s\n", "nsx."+p.Domain)
-	cert, err := cert.CreateCertificate("nsx."+p.Domain, "")
-	if err != nil {
-		return err
-	}
-
-	if err := p.CreateOrUpdateSecret("nsx-secret", Namespace, map[string][]byte{
-		"tls.crt": cert.EncodedCertificate(),
-		"tls.key": cert.EncodedPrivateKey(),
-	}); err != nil {
-		return err
-	}
-	// p.NSX.NsxV3.NsxApiCertFile = "/etc/nsx-ujo/nsx-cert/tls.crt"
-	// p.NSX.NsxV3.NsxApiPrivateKeyFile = "/etc/nsx-ujo/nsx-cert/tls.key"
-	yes := true
-	p.NSX.NsxV3.Insecure = &yes
-	p.NSX.NsxCOE.Cluster = p.Name
-
-	ini := structs.Map(p.NSX)
-
-	s := "[Defaults]\n" + mapToINI(ini)
-
-	log.Tracef("Using NSX config: %s", utils.StripSecrets(string(s)))
-
-	if err := p.CreateOrUpdateConfigMap("nsx-ncp-config", Namespace, map[string]string{
-		"ncp.ini": string(s),
-	}); err != nil {
-		return err
-	}
-
-	if err := p.CreateOrUpdateConfigMap("nsx-node-agent-config", Namespace, map[string]string{
-		"ncp.ini": string(s),
-	}); err != nil {
-		return err
-	}
-
-	return nil
 }
