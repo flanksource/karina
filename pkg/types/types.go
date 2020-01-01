33b439b9bc1386faf8a6e3a373115c7a7a4ee8f1
Moshe Immerman <moshe.immerman@gmail.com>
2020-01-01 18:58:34 +0200

    add platform version to faciliate the use of different versions

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -14,6 +14,7 @@ import (
 
 type PlatformConfig struct {
 	BootstrapToken        string            `yaml:"-"`
+	Version               string            `yaml:"version,omitempty"`
 	Backups               *Backups          `yaml:"backups,omitempty"`
 	Calico                Calico            `yaml:"calico,omitempty"`
 	Certificates          *Certificates     `yaml:"-"`
