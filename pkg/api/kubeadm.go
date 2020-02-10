9c60a671a4e5279165d6da3f879673ae8592067d
Teodor <teodor.pripoae@gmail.com>
2020-02-10 20:53:31 +0200

    Use ingressCA in api server

--- pkg/api/kubeadm.go
+++ pkg/api/kubeadm.go
@@ -1,5 +1,28 @@
 package api
 
+type HostPathType string
+
+const (
+	// For backwards compatible, leave it empty if unset
+	HostPathUnset HostPathType = ""
+	// If nothing exists at the given path, an empty directory will be created there
+	// as needed with file mode 0755, having the same group and ownership with Kubelet.
+	HostPathDirectoryOrCreate HostPathType = "DirectoryOrCreate"
+	// A directory must exist at the given path
+	HostPathDirectory HostPathType = "Directory"
+	// If nothing exists at the given path, an empty file will be created there
+	// as needed with file mode 0644, having the same group and ownership with Kubelet.
+	HostPathFileOrCreate HostPathType = "FileOrCreate"
+	// A file must exist at the given path
+	HostPathFile HostPathType = "File"
+	// A UNIX socket must exist at the given path
+	HostPathSocket HostPathType = "Socket"
+	// A character device must exist at the given path
+	HostPathCharDev HostPathType = "CharDevice"
+	// A block device must exist at the given path
+	HostPathBlockDev HostPathType = "BlockDevice"
+)
+
 type ClusterConfiguration struct {
 	APIVersion           string `yaml:"apiVersion,omitempty"`
 	Kind                 string `yaml:"kind"`
@@ -9,6 +32,7 @@ type ClusterConfiguration struct {
 		CertSANs               []string          `yaml:"certSANs,omitempty"`
 		TimeoutForControlPlane string            `yaml:"timeoutForControlPlane,omitempty"`
 		ExtraArgs              map[string]string `yaml:"extraArgs,omitempty"`
+		ExtraVolumes           []HostPathMount   `yaml:"extraVolumes,omitempty"`
 	} `yaml:"apiServer,omitempty"`
 	CertificatesDir   string `yaml:"certificatesDir,omitempty"`
 	ClusterName       string `yaml:"clusterName,omitempty"`
@@ -51,4 +75,18 @@ type BootstrapToken struct {
 
 type NodeRegistration struct {
 	KubeletExtraArgs map[string]string `yaml:"kubeletExtraArgs,omitempty"`
+}
+
+type HostPathMount struct {
+	// Name of the volume inside the pod template.
+	Name string `yaml:"name"`
+	// HostPath is the path in the host that will be mounted inside
+	// the pod.
+	HostPath string `yaml:"hostPath"`
+	// MountPath is the path inside the pod where hostPath will be mounted.
+	MountPath string `yaml:"mountPath"`
+	// ReadOnly controls write access to the volume
+	ReadOnly bool `yaml:"readOnly,omitempty"`
+	// PathType is the type of the HostPath.
+	PathType HostPathType `yaml:"pathType,omitempty"`
 }
