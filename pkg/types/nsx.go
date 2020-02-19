aad985a5e72c89af9ae8fff767bbe4bfbe440bc9
Moshe Immerman <moshe.immerman@gmail.com>
2020-02-19 15:48:51 +0200

    package/file layout refactoring

--- pkg/types/nsx.go
+++ pkg/types/nsx.go
@@ -10,7 +10,6 @@ type NSX struct {
 	// default INFO level.
 	Debug *bool `structs:"debug,omitempty" yaml:"debug,omitempty"`
 	// If set to true, log output to standard error.
-	//use_stderr = True
 	UseStderr *bool `structs:"use_stderr,omitempty" yaml:"use_stderr,omitempty"`
 
 	// If set to true, use syslog for logging.
@@ -27,15 +26,12 @@ type NSX struct {
 	LogRotationFileMaxMb *int `structs:"log_rotation_file_max_mb,omitempty" yaml:"log_rotation_file_max_mb,omitempty"`
 
 	// Total number of compressed backup files to store. Defaults to 5.
-	//log_rotation_backup_count = 5
 	LogRotationBackupCount *int `structs:"log_rotation_backup_count,omitempty" yaml:"log_rotation_backup_count,omitempty"`
 
 	// Specify the directory where nsx-python-logging is installed
-	// nsx_python_logging_path = /opt/vmware/nsx-common/python
 	NsxPythonLoggingPath string `structs:"nsx_python_logging_path,omitempty" yaml:"nsx_python_logging_path,omitempty"`
 
 	// Specify the directory where nsx-cli is installed
-	// nsx_cli_path = /opt/vmware/nsx-cli/bin/python
 	NsxCliPath string `structs:"nsx_cli_path,omitempty" yaml:"nsx_cli_path,omitempty"`
 
 	NsxV3 *NsxV3 `structs:"nsx_v3,omitempty" yaml:"nsx_v3,omitempty"`
@@ -49,12 +45,7 @@ type NSX struct {
 	NsxNodeAgent *NsxNodeAgent `structs:"nsx_node_agent" yaml:"nsx_node_agent,omitempty"`
 }
 
-// [DEFAULT]
-
 type NsxV3 struct {
-
-	// [nsx_v3]
-
 	NsxApiUser   string `structs:"nsx_api_user,omitempty" yaml:"nsx_api_user,omitempty"`
 	NsxApiPass   string `structs:"nsx_api_password,omitempty" yaml:"nsx_api_password,omitempty"`
 	PolicyNSXAPI *bool  `structs:"policy_nsxapi" yaml:"policy_nsxapi,omitempty"`
@@ -66,7 +57,6 @@ type NsxV3 struct {
 	// Path to NSX client private key file. If specified, the nsx_api_user and
 	// nsx_api_password options will be ignored. Must be specified along with
 	// nsx_api_cert_file option
-	//nsx_api_private_key_file = <None>
 	NsxApiPrivateKeyFile string `structs:"nsx_api_private_key_file,omitempty" yaml:"nsx_api_private_key_file,omitempty"`
 
 	// IP address of one or more NSX managers separated by commas. The IP
@@ -75,92 +65,74 @@ type NsxV3 struct {
 	// If
 	// scheme is not provided https is used. If port is not provided port 80 is
 	// used for http and port 443 for https.
-	//nsx_api_managers = []
 	NsxApiManagers []string `structs:"nsx_api_managers,omitempty" yaml:"nsx_api_managers,omitempty"`
 
 	// If True, skip fatal errors when no endpoint in the NSX management cluster
 	// is available to serve a request, and retry the request instead
-	//cluster_unavailable_retry = False
 	ClusterUnavailableRetry *bool `structs:"cluster_unavailable_retry,omitempty" yaml:"cluster_unavailable_retry,omitempty"`
 
 	// Maximum number of times to retry API requests upon stale revision errors.
-	//retries = 10
 	Retries *int `structs:"retries,omitempty" yaml:"retries,omitempty"`
 
 	// Specify one or a list of CA bundle files to use in verifying the NSX
 	// Manager server certificate. This option is ignored if "insecure" is set
 	// to True. If "insecure" is set to False and ca_file is unset, the system
 	// root CAs will be used to verify the server certificate.
-	//ca_file = []
 	CaFile []string `structs:"ca_file,omitempty" yaml:"ca_file,omitempty"`
 
 	// If true, the NSX Manager server certificate is not verified. If false the
 	// CA bundle specified via "ca_file" will be used or if unset the default
 	// system root CAs will be used.
-	//insecure = False
 	Insecure *bool `structs:"insecure,omitempty" yaml:"insecure,omitempty"`
 
 	// The time in seconds before aborting a HTTP connection to a NSX manager.
-	//http_timeout = 10
 	HttpTimeout *int `structs:"http_timeout,omitempty" yaml:"http_timeout,omitempty"`
+
 	// The time in seconds before aborting a HTTP read response from a NSX
 	// manager.
-	//http_read_timeout = 180
 	HttpReadTimeout *int `structs:"http_read_timeout,omitempty" yaml:"http_read_timeout,omitempty"`
 
 	// Maximum number of times to retry a HTTP connection.
-	//http_retries = 3
 	HttpRetries *int `structs:"http_retries,omitempty" yaml:"http_retries,omitempty"`
 
 	// Maximum concurrent connections to each NSX manager.
-	//concurrent_connections = 10
 	ConcurrentConnections *int `structs:"concurrent_connections,omitempty" yaml:"concurrent_connections,omitempty"`
 
 	// The amount of time in seconds to wait before ensuring connectivity to the
 	// NSX manager if no manager connection has been used.
-	//conn_idle_timeout = 10
 	ConnIdltTimeout *int `structs:"conn_idlt_timeout,omitempty" yaml:"conn_idlt_timeout,omitempty"`
 
 	// Number of times a HTTP redirect should be followed.
-	//redirects = 2
 	Redirects *int `structs:"redirects,omitempty" yaml:"redirects,omitempty"`
 
 	// Subnet prefix of IP block.
-	//subnet_prefix = 24
 	SubnetPrefix *int `structs:"subnet_prefix,omitempty" yaml:"subnet_prefix,omitempty"`
 
 	// Indicates whether distributed firewall DENY rules are logged.
-	//log_dropped_traffic = False
 	LogDroppedTraffic *bool `structs:"log_dropped_traffic,omitempty" yaml:"log_dropped_traffic,omitempty"`
 
 	// Option to use native load balancer or not
-	//use_native_loadbalancer = True
 	UseNativeLoadbalancer *bool `structs:"use_native_loadbalancer,omitempty" yaml:"use_native_loadbalancer,omitempty"`
 
 	// Option to auto scale layer 4 load balancer or not. If set to True, NCP
 	// will create additional LB when necessary upon K8s Service of type LB
 	// creation/update.
-	//l4_lb_auto_scaling = True
 	L4LBAutoScaling *bool `structs:"l_4_lb_auto_scaling,omitempty" yaml:"l_4_lb_auto_scaling,omitempty"`
 
 	// Option to use native load balancer or not when ingress class annotation
 	// is missing. Only effective if use_native_loadbalancer is set to true
-	//default_ingress_class_nsx = True
 	DefaultIngressClassNsx *bool `structs:"default_ingress_class_nsx,omitempty" yaml:"default_ingress_class_nsx,omitempty"`
 
 	// Path to the default certificate file for HTTPS load balancing. Must be
 	// specified along with lb_priv_key_path option
-	//lb_default_cert_path = <None>
 	LBDefaultCertPath string `structs:"lb_default_cert_path,omitempty" yaml:"lb_default_cert_path,omitempty"`
 
 	// Path to the private key file for default certificate for HTTPS load
 	// balancing. Must be specified along with lb_default_cert_path option
-	//lb_priv_key_path = <None>
 	LBPrivKeyPath string `structs:"lb_priv_key_path,omitempty" yaml:"lb_priv_key_path,omitempty"`
 
 	// Option to set load balancing algorithm in load balancer pool object.
 	// Choices: ROUND_ROBIN LEAST_CONNECTION IP_HASH WEIGHTED_ROUND_ROBIN
-	//pool_algorithm = ROUND_ROBIN
 	PoolAlgorithm string `structs:"pool_algorithm,omitempty" yaml:"pool_algorithm,omitempty"`
 
 	// Option to set load balancer service size. MEDIUM Edge VM (4 vCPU, 8GB)
@@ -168,32 +140,26 @@ type NsxV3 struct {
 	// and SMALL LB. Bare Metal Edge (IvyBridge, 2 socket, 128GB) supports
 	// LARGE, MEDIUM and SMALL LB
 	// Choices: SMALL MEDIUM LARGE
-	//service_size = SMALL
-
 	ServiceSize string `structs:"service_size,omitempty" yaml:"service_size,omitempty"`
 
 	// Option to set load balancer persistence option. If cookie is selected,
 	// cookie persistence will be offered.If source_ip is selected, source IP
 	// persistence will be offered for ingress traffic through L7 load balancer
 	// Choices: <None> cookie source_ip
-	//l7_persistence = <None>
 	L7Persistence string `structs:"l7_persistence,omitempty" yaml:"l7_persistence,omitempty"`
 
 	// An integer for LoadBalancer side timeout value in seconds on layer 7
 	// persistence profile, if the profile exists.
-	//l7_persistence_timeout = 10800
 	L7_PersistenceTimeout *int `structs:"l7_persistence_timeout,omitempty" yaml:"l7_persistence_timeout,omitempty"`
 
 	// Option to set load balancer persistence option. If source_ip is selected,
 	// source IP persistence will be offered for ingress traffic through L4 load
 	// balancer
-	// Choices: <None> source_ip
 	L4Persistence string `structs:"l4_persistence,omitempty" yaml:"l4_persistence,omitempty"`
 
 	// The interval to check VIF for node. It is a workaroud for bug 2006790.
 	// Old orphan LSP may not be removed on MP, so NCP will retrieve parent VIF
 	// back once in a while. NCP will use the last created LSP from the list
-	//vif_check_interval = 600
 	VIFCheckInterval *int `structs:"vif_check_interval,omitempty" yaml:"vif_check_interval,omitempty"`
 
 	// Name or UUID of the container ip blocks that will be used for creating
@@ -221,12 +187,10 @@ type NsxV3 struct {
 	// single_tier_topology is True and tier0_gateway is defined,
 	// top_tier_router value can be empty and a tier1 gateway is automatically
 	// created for the cluster
-	//top_tier_router = <None>
 	TopTierRouter string `structs:"top_tier_router,omitempty" yaml:"top_tier_router,omitempty"`
 
 	// Name or UUID of the external ip pools that will be used only for
 	// allocating IP addresses for Ingress controller and LB service
-	//external_ip_pools_lb = []
 	ExternalIPPoolsLB []string `structs:"external_ip_pools_lb,omitempty" yaml:"external_ip_pools_lb,omitempty"`
 
 	// Name or UUID of the NSX overlay transport zone that will be used for
@@ -234,11 +198,6 @@ type NsxV3 struct {
 	// already existing resource on NSX and every transport node where VMs
 	// hosting containers are deployed must be enabled on this transport zone
 	OverlayTZ string `structs:"overlay_tz,omitempty" yaml:"overlay_tz,omitempty"`
-
-	// Name or UUID of the lb service that can be attached by virtual servers
-	//lb_service = <None>
-
-	// LBService string
 
 	// Enable X_forward_for for ingress. Available values are INSERT or REPLACE.
 	// When this config is set, if x_forwarded_for is missing, LB will add
@@ -249,7 +208,6 @@ type NsxV3 struct {
 	// x_forwarded_for in the header. If not wanting to use x_forwarded_for,
 	// remove this config
 	// Choices: <None> INSERT REPLACE
-
 	XForwardedFor string `structs:"x_forwarded_for,omitempty" yaml:"x_forwarded_for,omitempty"`
 
 	// Name or UUID of the spoof guard switching profile that will be used by
@@ -267,12 +225,10 @@ type NsxV3 struct {
 	// Replication mode of container logical switch, set SOURCE for cloud as it
 	// only supports head replication mode
 	// Choices: MTEP SOURCE
-	//ls_replication_mode = MTEP
 	LSReplicationMode string `structs:"ls_replication_mode,omitempty" yaml:"ls_replication_mode,omitempty"`
 
 	// Allocate vlan ID for container interface or not. Set it to False for
 	// cloud mode.
-	//alloc_vlan_tag = True
 	AllocVlanTag string `structs:"alloc_vlan_tag,omitempty" yaml:"alloc_vlan_tag,omitempty"`
 
 	// The resource which NCP will search tag 'node_name' on, to get parent VIF
@@ -286,8 +242,7 @@ type NsxV3 struct {
 
 	// Determines which kind of information to be used as VIF app_id. Defaults
 	// to pod_resource_key. In WCP mode, pod_uid is used.
-	// Choices: pod_resource_key pod_uid]
-	//vif_app_id_type = pod_resource_key
+	// Choices: pod_resource_key pod_uid
 	VifAppIdType string `structs:"vif_app_id_type,omitempty" yaml:"vif_app_id_type,omitempty"`
 
 	// SNAT IP to secondary IPs mapping. In the cloud case, SNAT rules are
@@ -301,12 +256,10 @@ type NsxV3 struct {
 	DnsServers []string `structs:"dns_servers,omitempty" yaml:"dns_servers,omitempty"`
 
 	// Set this to True to enable NCP to report errors through NSXError CRD.
-	//enable_nsx_err_crd = False
 	EnableNsxErrCrd *bool `structs:"enable_nsx_err_crd,omitempty" yaml:"enable_nsx_err_crd,omitempty"`
 
 	// Maximum number of virtual servers allowed to create in cluster for
 	// LoadBalancer type of services.
-	//max_allowed_virtual_servers = 9223372036854775807
 	MaxAllowedVirtualServers *int `structs:"max_allowed_virtual_servers,omitempty" yaml:"max_allowed_virtual_servers,omitempty"`
 
 	// Edge cluster ID needed when creating Tier1 router for loadbalancer
@@ -316,20 +269,16 @@ type NsxV3 struct {
 
 type NsxHA struct {
 
-	// [ha]
-
 	// Time duration in seconds of mastership timeout. NCP instance will remain
 	// master for this duration after elected. Note that the heartbeat period
 	// plus the update timeout must not be greater than this period. This is
 	// done to ensure that the master instance will either confirm liveness or
 	// fail before the timeout.
-	//master_timeout = 18
 	MasterTimeout *int `structs:"master_timeout,omitempty"`
 
 	// Time in seconds between heartbeats for elected leader. Once an NCP
 	// instance is elected master, it will periodically confirm liveness based
 	// on this value.
-	//heartbeat_period = 6
 	HeartbeatPeriod *int `structs:"heartbeat_period,omitempty"`
 
 	// Timeout duration in seconds for update to election resource. The default
@@ -337,20 +286,15 @@ type NsxHA struct {
 	// If the update request does not complete before the timeout it will be
 	// aborted. Used for master heartbeats to ensure that the update fstructs:shes or
 	// is aborted before the master timeout occurs.
-	//update_timeout = <None>
 	UpdateTimeout *int `structs:"update_timeout,omitempty"`
 }
 
 type NsxCOE struct {
 
-	// [coe]
-
 	// Container orchestrator adaptor to plug in.
-	//adaptor = kubernetes
 	Adaptor string `structs:"adaptor,omitempty" yaml:"adaptor,omitempty"`
 
 	// Specify cluster for adaptor.
-	//cluster = k8scluster
 	Cluster string `structs:"cluster,omitempty" yaml:"cluster,omitempty"`
 
 	// Log level for NCP operations
@@ -359,11 +303,9 @@ type NsxCOE struct {
 
 	// Log level for NSX API client operations
 	// Choices: NOTSET DEBUG INFO WARNING ERROR CRITICAL
-	//nsxlib_loglevel = <None>
 	NsxlibLoglevel string `structs:"nsxlib_loglevel,omitempty" yaml:"nsxlib_loglevel,omitempty"`
 
 	// Enable SNAT for all projects in this cluster
-	//enable_snat = True
 	EnableSnat *bool `structs:"enable_snat,omitempty" yaml:"enable_snat,omitempty"`
 
 	// Option to enable profiling
@@ -371,20 +313,16 @@ type NsxCOE struct {
 
 	// The type of container host node
 	// Choices: HOSTVM BAREMETAL CLOUD WCP_WORKER
-	//node_type = HOSTVM
 	NodeType string `structs:"node_type,omitempty" yaml:"node_type,omitempty"`
 
 	// The time in seconds for NCP/nsx_node_agent to recover the connection to
 	// NSX manager/container orchestrator adaptor/Hyperbus before exiting. If
 	// the value is 0, NCP/nsx_node_agent wont exit automatically when the
 	// connection check fails
-	//connect_retry_timeout = 0
 	ConnectRetryTimeout *int `structs:"connect_retry_timeout,omitempty" yaml:"connect_retry_timeout,omitempty"`
 }
 
 type NsxK8s struct {
-	// [k8s]
-
 	// Kubernetes API server IP address.
 	ApiserverHostIp string `structs:"apiserver_host_ip,omitempty" yaml:"apiserver_host_ip,omitempty"`
 
@@ -393,29 +331,25 @@ type NsxK8s struct {
 
 	// Full path of the Token file to use for authenticating with the k8s API
 	// server.
-	// client_token_file = /var/run/secrets/kubernetes.io/serviceaccount/token
 	ClientTokenFile string `structs:"client_token_file,omitempty" yaml:"client_token_file,omitempty"`
 
 	// Full path of the client certificate file to use for authenticating with
 	// the k8s API server. It must be specified together with
 	// "client_private_key_file".
-	//client_cert_file = <None>
 	ClientCertFile string `structs:"client_cert_file,omitempty" yaml:"client_cert_file,omitempty"`
 
 	// Full path of the client private key file to use for authenticating with
 	// the k8s API server. It must be specified together with
-	// "client_cert_file".
+
 	ClientPrivateKeyFile string `structs:"client_private_key_file,omitempty" yaml:"client_private_key_file,omitempty"`
 
 	// Specify a CA bundle file to use in verifying the k8s API server
 	// certificate.
-	// ca_file = /var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 	CaFile string `structs:"ca_file,omitempty" yaml:"ca_file,omitempty"`
 
 	// Specify whether ingress controllers are expected to be deployed in
 	// hostnework mode or as regular pods externally accessed via NAT
 	// Choices: hostnetwork nat
-	//ingress_mode = hostnetwork
 	IngressMode string `structs:"ingress_mode,omitempty" yaml:"ingress_mode,omitempty"`
 
 	// Log level for the kubernetes adaptor
@@ -423,15 +357,13 @@ type NsxK8s struct {
 	Loglevel string `structs:"loglevel,omitempty" yaml:"loglevel,omitempty"`
 
 	// The default HTTP ingress port
-	//http_ingress_port = 80
+
 	HttpIngressPort *int `structs:"http_ingress_port,omitempty" yaml:"http_ingress_port,omitempty"`
 
 	// The default HTTPS ingress port
-	//https_ingress_port = 443
 	HttpsIngressPort *int `structs:"https_ingress_port,omitempty" yaml:"https_ingress_port,omitempty"`
 
 	// Specify thread pool size to process resource events
-	//resource_watcher_thread_pool_size = 1
 	ResourceWatcherThreadPoolSize *int `structs:"resource_watcher_thread_pool_size,omitempty" yaml:"resource_watcher_thread_pool_size,omitempty"`
 
 	// User specified IP address for HTTP and HTTPS ingresses
@@ -453,36 +385,21 @@ type NsxK8s struct {
 
 type NsxNodeAgent struct {
 
-	// # Prefix of node /proc path to mount on nsx_node_agent DaemonSet
-	// #proc_mount_path_prefix = /host
-
-	// # The log level of NSX RPC library
-	// # Choices: NOTSET DEBUG INFO WARNING ERROR CRITICAL
-	// #nsxrpc_loglevel = ERROR
+	// The log level of NSX RPC library
+	// Choices: NOTSET DEBUG INFO WARNING ERROR CRITICAL
 	LogLevel string `structs:"nsxrpc_loglevel,omitempty" yaml:"log_level,omitempty"`
-	// nsxrpc_loglevel
 
-	// # OVS bridge name
-	// #ovs_bridge = br-int
+	// OVS bridge name
 	OvsBridge string `structs:"ovs_bridge,omitempty" yaml:"ovs_bridge,omitempty"`
 
+	// The OVS uplink OpenFlow port where to apply the NAT rules to.
 	OvsUplinkPort string `structs:"ovs_uplink_port,omit_empty" yaml:"ovs_uplink_port,omitempty"`
 
-	// # The time in seconds for nsx_node_agent to wait CIF config from HyperBus
-	// # before returning to CNI
-	// #config_retry_timeout = 300
-
+	// The time in seconds for nsx_node_agent to wait CIF config from HyperBus
+	// before returning to CNI
 	ConfigRetryTimeout *int `structs:"config_retry_timeout,omitempty" yaml:"config_retry_timeout,omitempty"`
 
-	// # The time in seconds for nsx_node_agent to backoff before re-using an
-	// # existing cached CIF to serve CNI request. Must be less than
-	// # config_retry_timeout.
-	// #config_reuse_backoff_time = 15
+	// The time in seconds for nsx_node_agent to backoff before re-using an
+	// existing cached CIF to serve CNI request. Must be less than config_retry_timeout.
 	ConfigReuseBackoffTime *int `structs:"config_reuse_backoff_time,omitempty" yaml:"config_reuse_backoff_time,omitempty"`
-
-	// # The path for OpenvSwitch db socket
-	// #ovs_db_sock = unix:/var/run/openvswitch/db.sock
-
-	// # The OVS uplink OpenFlow port where to apply the NAT rules to.
-	// #ovs_uplink_port = <None>
 }
