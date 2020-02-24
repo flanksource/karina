a7d72f6f210dcfdf7fe4afd072f06c60d164298a
Moshe Immerman <moshe.immerman@gmail.com>
2020-02-24 17:35:06 +0200

    delete kind default storageclass

--- test/e2e.sh
+++ test/e2e.sh
@@ -38,6 +38,7 @@ $BIN deploy calico -v
 [[ -e ./test/install_certs.sh ]] && ./test/install_certs.sh
 
 .bin/kubectl -n kube-system set env daemonset/calico-node FELIX_IGNORELOOSERPF=true
+.bin/kubectl delete sc standard
 
 $BIN deploy base -v
 
