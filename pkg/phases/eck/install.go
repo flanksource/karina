316a9c59c3c79ca5031b607cc8d766bd65ff2d08
Moshe Immerman <moshe.immerman@gmail.com>
2020-02-25 20:16:16 +0200

    switch to embedded eck

--- pkg/phases/eck/install.go
+++ pkg/phases/eck/install.go
@@ -1,26 +1,24 @@
 package eck
 
 import (
-	"fmt"
-	"github.com/flanksource/commons/net"
-	"github.com/moshloop/platform-cli/pkg/platform"
-	log "github.com/sirupsen/logrus"
 	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/moshloop/platform-cli/pkg/platform"
 )
+
+const Namespace = "elastic-system"
 
 func Deploy(p *platform.Platform) error {
 	if p.ECK == nil || p.ECK.Disabled {
 		log.Infof("Skipping deployment of ECK, it is disabled")
 		return nil
 	} else {
-		log.Infof("Deploying ECK %s", p. ECK.Version)
+		log.Infof("Deploying ECK %s", p.ECK.Version)
 	}
-	if err := net.Download("https://download.elastic.co/downloads/eck/"+normalizeVersion(p.ECK.Version + "/all-in-one.yaml"), "build/eck.yaml"); err != nil {
-		return fmt.Errorf("deploy: failed to download ECK: %v", err)
-	}
-	kubectl := p.GetKubectl()
-	return kubectl("apply -f build/eck.yaml")
-	return nil
+
+	return p.ApplySpecs(Namespace, "eck.yml")
 }
 
 func normalizeVersion(version string) string {
