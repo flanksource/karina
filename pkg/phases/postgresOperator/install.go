04ff7abde58ea4bd71606c059afa5f24a50880e7
Moshe Immerman <moshe.immerman@gmail.com>
2020-03-08 13:56:40 +0200

    add default postgres operator version

--- pkg/phases/postgresOperator/install.go
+++ pkg/phases/postgresOperator/install.go
@@ -16,14 +16,15 @@ func Deploy(platform *platform.Platform) error {
 		return nil
 	}
 
+	if platform.PostgresOperator.Version == "" {
+		platform.PostgresOperator.Version = "v1.3.3"
+	}
 	if platform.PostgresOperator.SpiloImage == "" {
 		platform.PostgresOperator.SpiloImage = "docker.io/flanksource/spilo:1.6-p2.flanksource"
 	}
-
 	if platform.PostgresOperator.BackupImage == "" {
 		platform.PostgresOperator.BackupImage = "docker.io/flanksource/postgres-backups:0.1.5"
 	}
-
 	if platform.PostgresOperator.BackupBucket == "" {
 		platform.PostgresOperator.BackupBucket = "postgres-backups-" + platform.Name
 	}
