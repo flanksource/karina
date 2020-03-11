f6f6b3869b433eb0a250c079148559fd68da2021
Teodor <teodor.pripoae@gmail.com>
2020-03-11 16:09:20 +0200

    Fix tests for gitops

--- pkg/phases/flux/test.go
+++ pkg/phases/flux/test.go
@@ -14,11 +14,11 @@ func Test(p *platform.Platform, test *console.TestResults) {
 
 	k8s.TestNamespace(client, namespace, test)
 
-	pods, err := client.CoreV1().Pods(namespace).List(metav1.ListOptions{})
+	pods, err := client.CoreV1().Pods(namespace).List(metav1.ListOptions{LabelSelector: "app=nginx"})
 	if err != nil {
 		test.Failf("gitops", "Failed to list pods in namespace %s: %v", namespace, err)
 	} else if len(pods.Items) != 2 {
-		test.Failf("gitops", "Expected 2 pods in namespace %s got %d", namespace, len(pods.Items))
+		test.Failf("gitops", "Expected 2 nginx pods in namespace %s got %d", namespace, len(pods.Items))
 	} else {
 		test.Passf("gitops", "Pods for deployment nginx created successfully")
 	}
