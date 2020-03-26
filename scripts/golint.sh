3f38046dbbdffcaa42282fae70e71720770eaaa0
Moshe Immerman <moshe.immerman@gmail.com>
2020-03-26 19:58:51 +0200

    try and reduce golint memory usage

--- scripts/golint.sh
+++ scripts/golint.sh
@@ -3,5 +3,5 @@
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.24.0
 
 make pack && make
-
-GOGC=5 golangci-lint run --verbose --print-resources-usage
+mkdir -p test-results
+GOGC=1 golangci-lint run --verbose --print-resources-usage  --out-format=junit-xml > test-results/lint.xml
