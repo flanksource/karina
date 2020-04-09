9ed7385988da0c58ed9822af0de12280f40c94bd
Philip Stafford Wood <philip.wood@gmail.com>
2020-04-09 12:44:21 +0200

    trim e2e vsphere functionality for manual test

--- test/vsphere/e2e.sh
+++ test/vsphere/e2e.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#TODO: verify licensing origin then remove
 # Copyright 2020 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -21,44 +22,6 @@ set -o pipefail # any non-zero exit code in a piped command causes the pipeline 
 ARTIFACTS=$PWD/artifacts
 CERTS=$PWD/test/vsphere/openvpn/certs
 
-# Binary
-GO_VERSION=${GO_VERSION:-1.12}
-SOPS_VERSION=${SOPS_VERSION:-3.5.0}
-
-install_platformcli() {
-  docker run --rm -it -v "$PWD":"$PWD" -v "$PWD"/go:"$PWD"/go \
-    -u "$(id -u)":"$(id -g)" \
-    -w "$PWD" --entrypoint make \
-    -e GOPROXY=https://proxy.golang.org \
-    -e XDG_CACHE_HOME=/tmp/.cache \
-    golang:"$GO_VERSION" pack build
-
-  sudo mv "$PWD"/.bin/platform-cli /usr/local/bin/platform-cli
-  sudo chmod +x /usr/local/bin/platform-cli
-  sudo apt-get update
-  sudo apt-get install -y genisoimage
-}
-
-install_sops() {
-  curl -L https://github.com/mozilla/sops/releases/download/v"$SOPS_VERSION"/sops-v"$SOPS_VERSION".linux -o /tmp/sops
-  sudo mv /tmp/sops /usr/local/bin/sops
-  sudo chmod +x /usr/local/bin/sops
-}
-
-decrypt_vpn_certs() {
-  export AWS_ACCESS_KEY_ID=$SOPS_AWS_ACCESS_KEY_ID
-  export AWS_SECRET_ACCESS_KEY=$SOPS_AWS_SECRET_ACCESS_KEY
-
-  for filename in "$CERTS"/*.enc; do
-    # save the decrypted certs in the same folder w/o the ext .enc
-    sops --config "$PWD"/test/vsphere/.sops.yaml -d \
-      --input-type binary --output-type binary "$filename" >"${filename%.*}"
-  done
-
-  # clean a WARNING in openvpn
-  # WARNING: file '/openvpn/certs/user.key' is group or others accessible
-  chmod 400 "$CERTS"/user.key
-}
 
 dump_logs() {
   mkdir -p "$ARTIFACTS"/snapshot
@@ -68,20 +31,10 @@ dump_logs() {
 on_exit() {
 
   # remove the cluster
-  # shellcheck disable=SC2086
   platform-cli cleanup $PLATFORM_OPTIONS_FLAGS
-
-  # kill the VPN
-  docker kill vpn
 
   # clean certs dir - remove all except enc
   rm -fv "$CERTS"/{*.crt,*.key}
-}
-
-wait_for_vpn() {
-  while [ -n "$(ip addr show tun0 2>&1 >/dev/null)" ]; do
-    sleep 0.1
-  done
 }
 
 generate_cluster_id() {
@@ -101,65 +54,58 @@ export PLATFORM_OPTIONS_FLAGS="-e name=${PLATFORM_CLUSTER_ID} -e domain=${PLATFO
 
 export PLATFORM_CONFIG=$PWD/test/vsphere/e2e.yml
 
-# These variables are required to configure access to vSphere, they also work with govc
-export GOVC_FQDN="vcenter.lab.flanksource.com"
-export GOVC_DATACENTER="lab"
-export GOVC_CLUSTER="cluster"
-export GOVC_NETWORK="VM Network"
-export GOVC_PASS="${GOVC_PASS}"
-export GOVC_USER="${GOVC_USER}"
-export GOVC_DATASTORE="datastore1"
-export GOVC_INSECURE=1
+## These variables are required to configure access to vSphere, they also work with govc
+#export GOVC_FQDN="vcenter.lab.flanksource.com"
+#export GOVC_DATACENTER="lab"
+#export GOVC_CLUSTER="cluster"
+#export GOVC_NETWORK="VM Network"
+#export GOVC_PASS="${GOVC_PASS}"
+#export GOVC_USER="${GOVC_USER}"
+#export GOVC_DATASTORE="datastore1"
+#export GOVC_INSECURE=1
 export GOVC_URL="$GOVC_USER:$GOVC_PASS@$GOVC_FQDN"
 
 trap on_exit EXIT
 
-install_platformcli
+BIN=./.bin/platform-cli
+mkdir -p .bin
+export PLATFORM_CONFIG=test/common.yaml
+export GO_VERSION=${GO_VERSION:-1.13}
+export KUBECONFIG=~/.kube/config
 
-install_sops
+go version
 
-# Decrypt certs encrypted with SOPS and KMS Key from https://github.com/flanksource/vsphere-lab/blob/master/.sops.yaml
-decrypt_vpn_certs
+if go version | grep  go$GO_VERSION; then
+  make pack build
+else
+  docker run --rm -it -v $PWD:$PWD -v /go:/go -w $PWD --entrypoint make -e GOPROXY=https://proxy.golang.org golang:$GO_VERSION pack build
+fi
 
-# Run the vpn client in container
-docker run --rm -d --name vpn -v "${PWD}/test/vsphere/openvpn/:/openvpn/" \
-  -w "/openvpn/" --cap-add=NET_ADMIN --net=host --device=/dev/net/tun \
-  gcr.io/cluster-api-provider-vsphere/extra/openvpn:latest
+$BIN version
 
-echo "nameserver 10.255.0.2" | sudo tee -a /run/resolvconf/resolv.conf >/dev/null
-
-## Wait for VPN
-wait_for_vpn
-
-# Tail the vpn logs
-docker logs vpn
-
-# Generate Ingress certs
-platform-cli ca generate --name ingress-ca \
+# Generate ingress ca
+$BIN ca generate --name ingress-ca \
   --cert-path "$PLATFORM_CA" --private-key-path "$PLATFORM_PRIVATE_KEY" \
   --password "$PLATFORM_CA_CEK" --expiry 1
 
+
 # Create the cluster using the config from PLATFORM_CONFIG
-# shellcheck disable=SC2086
-platform-cli provision vsphere-cluster $PLATFORM_OPTIONS_FLAGS
+$BIN  provision vsphere-cluster $PLATFORM_OPTIONS_FLAGS
 
 # Install CNI
-# shellcheck disable=SC2086
-platform-cli deploy calico $PLATFORM_OPTIONS_FLAGS
+$BIN  deploy calico $PLATFORM_OPTIONS_FLAGS
 
 components=("base" "stubs" "all")
 for component in "${components[@]}"; do
   # Deploy the platform configuration
-  # shellcheck disable=SC2086
-  platform-cli deploy "$component" $PLATFORM_OPTIONS_FLAGS
+  $BIN deploy "$component" $PLATFORM_OPTIONS_FLAGS
 done
 
 set +o errexit # test failures are reported by Junit
 
 # Run conformance tests
-# shellcheck disable=SC2086
 failed=false
-if ! platform-cli test all $PLATFORM_OPTIONS_FLAGS --wait 240  --junit-path test-results/results.xml; then
+if ! $BIN test all $PLATFORM_OPTIONS_FLAGS --wait 240  --junit-path test-results/results.xml; then
    failed=true
 fi
 
