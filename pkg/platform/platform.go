5efd23ddbd64ac05e26e374149ee387e88b71cee
Philip Stafford Wood <philip.wood@gmail.com>
2020-04-11 22:28:07 +0200

    error handling for missing ingress and root ca files and some godocs

--- pkg/platform/platform.go
+++ pkg/platform/platform.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"crypto/tls"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -74,6 +75,9 @@ func (platform *Platform) GetKubeConfigBytes() ([]byte, error) {
 	return k8s.CreateKubeConfig(platform.Name, platform.GetCA(), masters[0], "system:masters", "admin")
 }
 
+// GetCA retrieves the cert.CertificateAuthority
+// for the given platform, initialising it (platform.ca) if it hasn't been read from
+// the specified config (platform.CA) yet.
 func (platform *Platform) GetCA() certs.CertificateAuthority {
 	if platform.ca != nil {
 		return platform.ca
@@ -86,9 +90,17 @@ func (platform *Platform) GetCA() certs.CertificateAuthority {
 	return ca
 }
 
+// readCA opens the CA stored in the file ca.Cert using the private key in ca.PrivateKey
+// with key password ca.Password.
 func readCA(ca *types.CA) (*certs.Certificate, error) {
 	cert := files.SafeRead(ca.Cert)
+	if cert == "" {
+		return nil, errors.New(fmt.Sprintf("Unable to read certificate %s", ca.Cert))
+	}
 	privateKey := files.SafeRead(ca.PrivateKey)
+	if privateKey == "" {
+		return nil, errors.New(fmt.Sprintf("Unable to read private key %s", ca.PrivateKey))
+	}
 	return certs.DecryptCertificate([]byte(cert), []byte(privateKey), []byte(ca.Password))
 }
 
