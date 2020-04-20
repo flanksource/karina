d607eaf4f4e2a81b2cf918c59e79ef8c2125681f
Teodor <teodor.pripoae@gmail.com>
2020-04-20 21:05:46 +0300

    Exit if checksums are not matching

--- scripts/golint.sh
+++ scripts/golint.sh
@@ -14,10 +14,12 @@ NEW_GO_SUM_CHECKSUM=$(sha1sum go.sum | awk -F " " '{print $1}')
 
 if [ "$GO_MOD_CHECKSUM" != "$NEW_GO_MOD_CHECKSUM" ]; then
   echo "go.mod is not up to date. Checksum before 'go mod tidy' was $GO_MOD_CHECKSUM and after was $NEW_GO_MOD_CHECKSUM"
+  exit 1
 fi
 
 if [ "$GO_SUM_CHECKSUM" != "$NEW_GO_SUM_CHECKSUM" ]; then
   echo "go.sum is not up to date. Checksum before 'go mod tidy' was $GO_GO_SUM_CHECKSUM and after was $NEW_GO_SUM_CHECKSUM"
+  exit 1
 fi
 
 # Check that no *.yml files are present, only use *.yaml as standard suggests
