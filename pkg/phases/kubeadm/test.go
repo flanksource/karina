a808b7a213d1ed67c4f4ea8d0789b0f2caa138bf
Philip Stafford Wood <philip.wood@gmail.com>
2020-04-23 23:31:33 +0200

    add command tests for encryption configuration

--- pkg/phases/kubeadm/test.go
+++ pkg/phases/kubeadm/test.go
@@ -73,58 +73,49 @@ func TestEncryption(p *platform.Platform, tr *console.TestResults) {
 		return
 	}
 
-	//pod, err := p.Client.GetFirstPodByLabelSelector("kube-system", "component=kube-apiserver")
-	//if err != nil {
-	//	tr.Failf(testEncryptionName, "Failed to get api-server pod: %v", err)
-	//	return
-	//}
-	//tr.Passf(testEncryptionName, "api-server pod found")
-
 	etcdPod, err := p.Client.GetFirstPodByLabelSelector("kube-system", "component=etcd")
 	if err != nil {
-		tr.Failf(testEncryptionName, "Failed to get api-server pod: %v", err)
+		tr.Failf(testEncryptionName, "Failed to get etcd pod: %v", err)
 		return
 	}
-	tr.Passf(testEncryptionName, "etcd pod found")
+	tr.Infof("ETCD pod '%v' found", etcdPod.Name)
 
-	//if logFilePath, ok := p.Kubernetes.APIServerExtraArgs["encryption-provider-config"]; !ok {
-	//	tr.Failf(testEncryptionName, "No encryption-provider-config is specified!")
-	//	return
-	//} else if logFilePath == "" {
-	//	tr.Failf(testEncryptionName, "Empty encryption-provider-config is specified!")
-	//	return
-	//} else {
-		//TODO: test encryption is working
-		tr.Infof("%v",etcdPod)
-		ns := "default"
-		secretName := "encryption-test-secret"
-		secretKey := "test-secret"
-		secretValue := "correct-horse-battery-staple"
-		tr.Infof("Creating secret %v in %v",secretName, ns)
-		p.Client.CreateOrUpdateSecret(secretName,ns,
-			map[string]([]byte){
-				secretKey: []byte(secretValue),
-			})
+	ns := "default"
+	secretName := "encryption-test-secret"
+	secretKey := "test-secret"
+	secretValue := "correct-horse-battery-staple"
+	tr.Infof("Creating secret '%v' in namespace '%v'", secretName, ns)
+	p.Client.CreateOrUpdateSecret(secretName, ns,
+		map[string]([]byte){
+			secretKey: []byte(secretValue),
+		})
 
-		verificationCommand := fmt.Sprintf("ETCDCTL_API=3 etcdctl get /registry/secrets/%v/%v"+
-			" --endpoints https://127.0.0.1:2379"+
-			" --cacert /etc/kubernetes/pki/etcd/ca.crt" +
-			" --cert /etc/kubernetes/pki/etcd/peer.crt" +
-			" --key /etc/kubernetes/pki/etcd/peer.key" +
-			" | strings -n 6 -", ns,secretName)
-		stdout, stderr, err := p.ExecutePodf("kube-system", etcdPod.Name, "etcd",
-			"/bin/sh", "-c",
-			// one long '-quoted command passed to /bin/sh
-			"'"+verificationCommand+"'")
-		// e.g.:
-		// kubectl exec -n kube-system etcd-kind-control-plane -- /bin/sh -c 'ETCDCTL_API=3 etcdctl get /registry/secrets/default/secret1 --endpoints https://127.0.0.1:2379 --cacert /etc/kubernetes/pki/etcd/ca.crt --cert /etc/kubernetes/pki/etcd/peer.crt --key /etc/kubernetes/pki/etcd/peer.key | strings -n 6 -'
-		///registry/secrets/default/secret1
-		//k8s:enc:aescbc:v1:demokey:9
-		if err != nil || stderr != "" {
-			tr.Failf(testEncryptionName, "Failed to verify secret: %v\n%v", err, stderr)
-		} else if strings.Contains(stdout,"k8s:enc:aescbc:v1" ) {
-			tr.Passf(testEncryptionName, " %v", stdout)
-			//TODO: read secret to be sure
-		}
-	//}
+	verificationCommand := fmt.Sprintf("ETCDCTL_API=3 etcdctl get /registry/secrets/%v/%v"+
+		" --endpoints https://127.0.0.1:2379"+
+		" --cacert /etc/kubernetes/pki/etcd/ca.crt"+
+		" --cert /etc/kubernetes/pki/etcd/peer.crt"+
+		" --key /etc/kubernetes/pki/etcd/peer.key"+
+		" | strings -n 6 -", ns, secretName)
+	tr.Debugf("verificationCommand: %v", verificationCommand)
+	stdout, stderr, err := p.ExecutePodf("kube-system", etcdPod.Name, "etcd",
+		"/bin/sh", "-c", verificationCommand)
+	if err != nil || stderr != "" {
+		tr.Failf(testEncryptionName, "Failed to verify secret: %v\n%v", err, stderr)
+		return
+	}
+	if strings.Contains(stdout, "k8s:enc:aescbc:v1") {
+		tr.Passf(testEncryptionName, "ETCD reports key %v is encrypted", strings.ReplaceAll(stdout, "\n", ""))
+	}
+	s := p.Client.GetSecret(ns, secretName)
+	got, ok := (*s)[secretKey]
+	if !ok {
+		tr.Failf(testEncryptionName, "Failed to find secret key %v in secret %v", secretKey, secretName)
+		return
+	}
+	if string(got) == secretValue {
+		tr.Passf(testEncryptionName, "Secret %v is readable", secretName)
+	} else {
+		tr.Failf(testEncryptionName, "Secret %v could not be read, wanted %v, got %v", secretName, secretValue, got)
+	}
+	return
 }
