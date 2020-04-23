6725abc3e2a1d4c53da1e1bf7de02d1c356e7d55
Philip Stafford Wood <philip.wood@gmail.com>
2020-04-23 11:41:30 +0200

    add component test scaffold and fixture for encryption

--- pkg/phases/kubeadm/test.go
+++ pkg/phases/kubeadm/test.go
@@ -7,7 +7,10 @@ import (
 	"github.com/moshloop/platform-cli/pkg/platform"
 )
 
-const testAuditName = "auditing"
+const (
+	testAuditName      = "auditing"
+	testEncryptionName = "encryption"
+)
 
 // Test k8s auditing functionality.
 func TestAudit(p *platform.Platform, tr *console.TestResults) {
@@ -49,5 +52,39 @@ func TestAudit(p *platform.Platform, tr *console.TestResults) {
 		} else {
 			tr.Passf(testAuditName, "api-server pod log size is: %v", stdout)
 		}
+	}
+}
+
+// Test k8s encryption provider functionality.
+func TestEncryption(p *platform.Platform, tr *console.TestResults) {
+	tc := p.Kubernetes.EncryptionConfig.EncryptionProviderConfigFile
+
+	if tc == "" {
+		tr.Skipf(testEncryptionName, "No encryption provider configuration specified.")
+		return
+	}
+
+	_, err := p.GetClientset()
+	if err != nil {
+		tr.Failf(testEncryptionName, "Failed to get k8s client: %v", err)
+		// We're done, we can't test anything further.
+		return
+	}
+
+	pod, err := p.Client.GetFirstPodByLabelSelector("kube-system", "component=kube-apiserver")
+	if err != nil {
+		tr.Failf(testEncryptionName, "Failed to get api-server pod: %v", err)
+		return
+	}
+	tr.Passf(testEncryptionName, "api-server pod found")
+
+	if logFilePath, ok := p.Kubernetes.APIServerExtraArgs["encryption-provider-config"]; !ok {
+		tr.Failf(testEncryptionName, "No encryption-provider-config is specified!")
+		return
+	} else if logFilePath == "" {
+		tr.Failf(testEncryptionName, "Empty encryption-provider-config is specified!")
+		return
+	} else {
+		//TODO: test encryption is working
 	}
 }
