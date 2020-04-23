33e1f368218b343bd3771702c24003f90e347727
Philip Stafford Wood <philip.wood@gmail.com>
2020-04-24 00:14:11 +0200

    repair e2e

--- test/e2e.sh
+++ test/e2e.sh
@@ -37,36 +37,36 @@ $BIN version
 
 $BIN deploy phases --base --stubs --dex --calico -v
 
-#[[ -e ./test/install_certs.sh ]] && ./test/install_certs.sh
-#
-## wait for the base deployment with stubs to come up healthy
-#$BIN test phases --base --stubs --wait 120 --progress=false
-#
-#$BIN deploy phases --vault --postgres-operator -v
-#
-#$BIN vault init -v
-#
-#$BIN deploy all -v
-#
-## deploy the opa bundles first, as they can take some time to load, this effectively
-## parallelizes this work to make the entire test complete faster
-#$BIN opa bundle automobile -v
-## wait for up to 4 minutes, rerunning tests if they fail
-## this allows for all resources to reconcile and images to finish downloading etc..
-#$BIN test all -v --wait 240 --progress=false
-#
-#failed=false
-#
-## e2e do not use --wait at the run level, if needed each individual test implements
-## its own wait. e2e tests should always pass once the non e2e have passed
-#if ! $BIN test all --e2e --progress=false -v --junit-path test-results/results.xml; then
-#  failed=true
-#fi
-#
-#mkdir -p artifacts
-#$BIN snapshot --output-dir snapshot -v --include-specs=true --include-logs=true --include-events=true
-#zip -r artifacts/snapshot.zip snapshot/*
-#
-#if [[ "$failed" = true ]]; then
-#  exit 1
-#fi
+[[ -e ./test/install_certs.sh ]] && ./test/install_certs.sh
+
+# wait for the base deployment with stubs to come up healthy
+$BIN test phases --base --stubs --wait 120 --progress=false
+
+$BIN deploy phases --vault --postgres-operator -v
+
+$BIN vault init -v
+
+$BIN deploy all -v
+
+# deploy the opa bundles first, as they can take some time to load, this effectively
+# parallelizes this work to make the entire test complete faster
+$BIN opa bundle automobile -v
+# wait for up to 4 minutes, rerunning tests if they fail
+# this allows for all resources to reconcile and images to finish downloading etc..
+$BIN test all -v --wait 240 --progress=false
+
+failed=false
+
+# e2e do not use --wait at the run level, if needed each individual test implements
+# its own wait. e2e tests should always pass once the non e2e have passed
+if ! $BIN test all --e2e --progress=false -v --junit-path test-results/results.xml; then
+  failed=true
+fi
+
+mkdir -p artifacts
+$BIN snapshot --output-dir snapshot -v --include-specs=true --include-logs=true --include-events=true
+zip -r artifacts/snapshot.zip snapshot/*
+
+if [[ "$failed" = true ]]; then
+  exit 1
+fi
