a321e203e20c5f5516c4d20b3872c204cb1c0721
Teodor <teodor.pripoae@gmail.com>
2020-05-19 20:39:18 +0300

    Remove cmd.template function, use !!env for environment variables

--- cmd/common.go
+++ cmd/common.go
@@ -3,12 +3,9 @@ package cmd
 import (
 	"fmt"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
-
-	"github.com/pkg/errors"
 
 	"github.com/flanksource/commons/console"
 	"github.com/flanksource/commons/is"
@@ -16,13 +13,13 @@ import (
 	"github.com/flanksource/commons/lookup"
 	"github.com/flanksource/commons/text"
 	"github.com/imdario/mergo"
-	log "github.com/sirupsen/logrus"
-	"github.com/spf13/cobra"
-	"gopkg.in/flanksource/yaml.v3"
-
 	"github.com/moshloop/platform-cli/pkg/phases/harbor"
 	"github.com/moshloop/platform-cli/pkg/platform"
 	"github.com/moshloop/platform-cli/pkg/types"
+	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	yaml "gopkg.in/flanksource/yaml.v3"
 )
 
 func getPlatform(cmd *cobra.Command) *platform.Platform {
@@ -70,83 +67,18 @@ func NewConfig(paths []string, extras []string) types.PlatformConfig {
 		log.Fatalf("Failed to merge default config, %v", err)
 	}
 
-	base.S3.AccessKey = template(base.S3.AccessKey)
-	base.S3.SecretKey = template(base.S3.SecretKey)
-
 	ldap := base.Ldap
 	if ldap.Port == "" {
 		ldap.Port = "636"
 	}
 	if ldap != nil {
-		ldap.Username = template(ldap.Username)
-		ldap.Password = template(ldap.Password)
 		base.Ldap = ldap
-	}
-
-	base.Master.Network = templateSlice(base.Master.Network)
-	base.Master.Cluster = template(base.Master.Cluster)
-	base.Master.Template = template(base.Master.Template)
-
-	nodes := base.Nodes
-	for name, vm := range base.Nodes {
-		vm.Network = templateSlice(vm.Network)
-		vm.Cluster = template(vm.Cluster)
-		vm.Template = template(vm.Template)
-		nodes[name] = vm
-	}
-	base.Nodes = nodes
-
-	dns := base.DNS
-	if dns != nil {
-		dns.Key = template(dns.Key)
-		dns.KeyName = template(dns.KeyName)
-		dns.AccessKey = template(dns.AccessKey)
-		dns.SecretKey = template(dns.SecretKey)
-		base.DNS = dns
 	}
 
 	if base.TrustedCA != "" && !is.File(base.TrustedCA) {
 		base.TrustedCA = text.ToFile(base.TrustedCA, ".pem")
 	}
 
-	if base.NSX != nil && base.NSX.NsxV3 != nil {
-		base.NSX.NsxV3.NsxAPIUser = template(base.NSX.NsxV3.NsxAPIUser)
-		base.NSX.NsxV3.NsxAPIPass = template(base.NSX.NsxV3.NsxAPIPass)
-	}
-
-	if base.CA != nil {
-		base.CA.Password = template(base.CA.Password)
-	}
-	if base.IngressCA != nil {
-		base.IngressCA.Password = template(base.IngressCA.Password)
-	}
-
-	if base.FluentdOperator != nil {
-		base.FluentdOperator.Elasticsearch.Password = template(base.FluentdOperator.Elasticsearch.Password)
-	}
-
-	for i := range base.Filebeat {
-		if base.Filebeat[i].Elasticsearch != nil {
-			base.Filebeat[i].Elasticsearch.Password = template(base.Filebeat[i].Elasticsearch.Password)
-		}
-	}
-
-	if base.Vault != nil {
-		base.Vault.AccessKey = template(base.Vault.AccessKey)
-		base.Vault.SecretKey = template(base.Vault.SecretKey)
-		base.Vault.KmsKeyID = template(base.Vault.KmsKeyID)
-		base.Vault.Token = template(base.Vault.Token)
-	}
-
-	if base.CertManager.Vault != nil {
-		base.CertManager.Vault.Token = template(base.CertManager.Vault.Token)
-	}
-
-	gitops := base.GitOps
-	for i := range gitops {
-		gitops[i].GitKey = template(gitops[i].GitKey)
-	}
-	base.GitOps = gitops
 	for _, extra := range extras {
 		key := strings.Split(extra, "=")[0]
 		val := extra[len(key)+1:]
@@ -231,27 +163,4 @@ var Render = &cobra.Command{
 		data, _ := yaml.Marshal(base)
 		fmt.Println(string(data))
 	},
-}
-
-func templateSlice(vals []string) []string {
-	var out []string
-	for _, val := range vals {
-		if strings.HasPrefix(val, "$") {
-			env := os.Getenv(val[1:])
-			out = append(out, strings.Split(env, ",")...)
-		} else {
-			out = append(out, strings.Split(val, ",")...)
-		}
-	}
-	return out
-}
-
-func template(val string) string {
-	if strings.HasPrefix(val, "$") {
-		env := os.Getenv(val[1:])
-		if env != "" {
-			return env
-		}
-	}
-	return val
 }
