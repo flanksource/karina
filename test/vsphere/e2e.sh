47b9cb3329567a12314901ea02ac9623e80eccd7
Philip Stafford Wood <philip.wood@gmail.com>
2020-05-25 11:51:55 +0200

    explicitly handle kubeconfig

--- test/vsphere/e2e.sh
+++ test/vsphere/e2e.sh
@@ -42,14 +42,16 @@ else
   docker run --rm -it -v $PWD:$PWD -v /go:/go -w $PWD --entrypoint make -e GOPROXY=https://proxy.golang.org golang:$GO_VERSION pack build
 fi
 
-if [[ "$KUBECONFIG" != "$HOME/.kube/kind-config-kind" ]] ; then
-  $BIN ca generate --name root-ca --cert-path .certs/root-ca.crt --private-key-path .certs/root-ca.key --password foobar  --expiry 1
-  $BIN ca generate --name ingress-ca --cert-path .certs/ingress-ca.crt --private-key-path .certs/ingress-ca.key --password foobar  --expiry 1
-  $BIN ca generate --name sealed-secrets --cert-path .certs/sealed-secrets-crt.pem --private-key-path .certs/sealed-secrets-key.pem --password foobar  --expiry 1
-  $BIN provision vsphere-cluster || exit 1
-fi
+
+$BIN ca generate --name root-ca --cert-path .certs/root-ca.crt --private-key-path .certs/root-ca.key --password foobar  --expiry 1
+$BIN ca generate --name ingress-ca --cert-path .certs/ingress-ca.crt --private-key-path .certs/ingress-ca.key --password foobar  --expiry 1
+$BIN ca generate --name sealed-secrets --cert-path .certs/sealed-secrets-crt.pem --private-key-path .certs/sealed-secrets-key.pem --password foobar  --expiry 1
+$BIN provision vsphere-cluster || exit 1
 
 $BIN version
+
+$BIN kubeconfig admin -c test/vsphere/e2e.yaml > config.yaml
+export KUBECONFIG=$PWD/config.yaml
 
 $BIN deploy phases --calico -v
 $BIN deploy phases --base -v
