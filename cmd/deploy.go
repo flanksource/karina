ab3e366073fc7fbc336412c212de7d898cfeb705
Teodor <teodor.pripoae@gmail.com>
2020-06-03 15:33:24 +0300

    Add beats to deploy phases, fix Disabled/Enabled config types

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	log "github.com/flanksource/commons/logger"
+	"github.com/flanksource/karina/pkg/phases/auditbeat"
 	"github.com/flanksource/karina/pkg/phases/base"
 	"github.com/flanksource/karina/pkg/phases/calico"
 	"github.com/flanksource/karina/pkg/phases/certmanager"
@@ -11,13 +12,16 @@ import (
 	"github.com/flanksource/karina/pkg/phases/dex"
 	"github.com/flanksource/karina/pkg/phases/eck"
 	"github.com/flanksource/karina/pkg/phases/elasticsearch"
+	"github.com/flanksource/karina/pkg/phases/eventrouter"
 	"github.com/flanksource/karina/pkg/phases/filebeat"
 	"github.com/flanksource/karina/pkg/phases/fluentdoperator"
 	"github.com/flanksource/karina/pkg/phases/flux"
 	"github.com/flanksource/karina/pkg/phases/harbor"
+	"github.com/flanksource/karina/pkg/phases/journalbeat"
 	"github.com/flanksource/karina/pkg/phases/monitoring"
 	"github.com/flanksource/karina/pkg/phases/nsx"
 	"github.com/flanksource/karina/pkg/phases/opa"
+	"github.com/flanksource/karina/pkg/phases/packetbeat"
 	"github.com/flanksource/karina/pkg/phases/platformoperator"
 	"github.com/flanksource/karina/pkg/phases/postgresoperator"
 	"github.com/flanksource/karina/pkg/phases/registrycreds"
@@ -35,19 +39,23 @@ import (
 type DeployFn func(p *platform.Platform) error
 
 var Phases = map[string]DeployFn{
+	"auditbeat":          auditbeat.Deploy,
 	"base":               base.Install,
 	"calico":             calico.Install,
 	"configmap-reloader": configmapreloader.Deploy,
 	"dex":                dex.Install,
 	"eck":                eck.Deploy,
 	"elasticsearch":      elasticsearch.Deploy,
+	"eventrouter":        eventrouter.Deploy,
 	"fluentd":            fluentdoperator.Deploy,
 	"filebeat":           filebeat.Deploy,
 	"gitops":             flux.Install,
 	"harbor":             harbor.Deploy,
+	"journalbeat":        journalbeat.Deploy,
 	"monitoring":         monitoring.Install,
 	"opa":                opa.Install,
 	"nsx":                nsx.Install,
+	"packetbeat":         packetbeat.Deploy,
 	"postgres-operator":  postgresoperator.Deploy,
 	"registry-creds":     registrycreds.Install,
 	"s3-upload-cleaner":  s3uploadcleaner.Deploy,
