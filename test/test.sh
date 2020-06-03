33b3540877d667619a3bac3b39e38945e92a4a70
Moshe Immerman <moshe.immerman@gmail.com>
2020-06-03 20:21:54 +0200

    switch to github actions

---
+++ test/test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+BIN=./.bin/karina
+mkdir -p .bin
+export GO_VERSION=${GO_VERSION:-1.13}
+export KUBECONFIG=~/.kube/config
+REPO=$(basename $(git remote get-url origin | sed 's/\.git//'))
+GITHUB_OWNER=$(basename $(dirname $(git remote get-url origin | sed 's/\.git//')))
+GITHUB_OWNER=${GITHUB_OWNER##*:}
+MASTER_HEAD=$(curl https://api.github.com/repos/$GITHUB_OWNER/$REPO/commits/master | jq -r '.sha')
+
+
+export PLATFORM_CONFIG=$SUITE.yaml
+
+if [[ "$KUBECONFIG" != "$HOME/.kube/kind-config-kind" ]] ; then
+  $BIN ca generate --name root-ca --cert-path .certs/root-ca.crt --private-key-path .certs/root-ca.key --password foobar  --expiry 1
+  $BIN ca generate --name ingress-ca --cert-path .certs/ingress-ca.crt --private-key-path .certs/ingress-ca.key --password foobar  --expiry 1
+  $BIN ca generate --name sealed-secrets --cert-path .certs/sealed-secrets-crt.pem --private-key-path .certs/sealed-secrets-key.pem --password foobar  --expiry 1
+  $BIN provision kind-cluster || exit 1
+fi
+
+$BIN version
+
+$BIN deploy phases --base --stubs --dex --calico -v
+
+[[ -e ./test/install_certs.sh ]] && ./test/install_certs.sh
+
+# wait for the base deployment with stubs to come up healthy
+$BIN test phases --base --stubs --wait 120 --progress=false
+
+$BIN deploy all -v
+
+# wait for up to 4 minutes, rerunning tests if they fail
+# this allows for all resources to reconcile and images to finish downloading etc..
+$BIN test all -v --wait 240 --progress=false
+
+failed=false
+
+# e2e do not use --wait at the run level, if needed each individual test implements
+# its own wait. e2e tests should always pass once the non e2e have passed
+if ! $BIN test all --e2e --progress=false -v --junit-path test-results/results.xml; then
+  failed=true
+fi
+
+mkdir -p artifacts
+$BIN snapshot --output-dir snapshot -v --include-specs=true --include-logs=true --include-events=true
+zip -r artifacts/snapshot.zip snapshot/*
+
+if [[ "$failed" = true ]]; then
+  exit 1
+fi
