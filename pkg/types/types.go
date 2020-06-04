1ef5bcefe686b091fa2434dbff2a06b185251a40
Moshe Immerman <moshe.immerman@gmail.com>
2020-06-04 11:46:04 +0200

    remove unused fields

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -85,15 +85,14 @@ type Calico struct {
 }
 
 type OPA struct {
-	Disabled           bool     `yaml:"disabled,omitempty"`
-	NamespaceWhitelist []string `yaml:"namespaceWhitelist,omitempty"`
-	KubeMgmtVersion    string   `yaml:"kubeMgmtVersion,omitempty"`
-	Version            string   `yaml:"version,omitempty"`
-	BundleURL          string   `yaml:"bundleUrl,omitempty"`
-	BundlePrefix       string   `yaml:"bundlePrefix,omitempty"`
-	BundleServiceName  string   `yaml:"bundleServiceName,omitempty"`
-	LogFormat          string   `yaml:"logFormat,omitempty"`
-	SetDecisionLogs    bool     `yaml:"setDecisionLogs,omitempty"`
+	Disabled          bool   `yaml:"disabled,omitempty"`
+	KubeMgmtVersion   string `yaml:"kubeMgmtVersion,omitempty"`
+	Version           string `yaml:"version,omitempty"`
+	BundleURL         string `yaml:"bundleUrl,omitempty"`
+	BundlePrefix      string `yaml:"bundlePrefix,omitempty"`
+	BundleServiceName string `yaml:"bundleServiceName,omitempty"`
+	LogFormat         string `yaml:"logFormat,omitempty"`
+	SetDecisionLogs   bool   `yaml:"setDecisionLogs,omitempty"`
 	// Policies is a path to directory containing .rego policy files
 	Policies string `yaml:"policies,omitempty"`
 	// Log level for opa server, one of: `debug`,`info`,`error` (default: `error`)
@@ -523,7 +522,11 @@ type Packetbeat struct {
 }
 
 type EventRouter struct {
+<<<<<<< Updated upstream
 	Disabled       `yaml:",inline"`
+=======
+	DisabledValue  `yaml:"inline"`
+>>>>>>> Stashed changes
 	FilebeatPrefix string `yaml:"filebeatPrefix"`
 }
 
