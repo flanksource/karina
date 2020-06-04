569f103d95bdb4f25be6ec146eaed9b46c1c629c
Philip Stafford Wood <philip.wood@gmail.com>
2020-06-04 15:28:54 +0200

    add cobra command for force-delete

---
+++ cmd/namespace.go
@@ -0,0 +1,49 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/hako/durafmt"
+
+	"github.com/spf13/cobra"
+
+	"time"
+)
+
+var Namespace = &cobra.Command{
+	Use:   "namespace",
+	Short: "Commands for manipulating namespaces",
+}
+
+var timeout time.Duration
+
+var namespaceForceDelete = &cobra.Command{
+	Use:   "force-delete",
+	Short: "Clears the namespace finalizers allowing it to be deleted",
+	Args:  cobra.MinimumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		p := getPlatform(cmd)
+
+		ns := args[0]
+		fmt.Printf("Clearing finalizers on namespace %v\n", ns)
+
+		duration := durafmt.Parse(timeout).String()
+		fmt.Printf("Using timeout of %v\n", duration)
+
+		err := p.ForceDeleteNamespace(ns, timeout)
+		if err != nil {
+			return fmt.Errorf("clearing the finalizers failed with: %v", err)
+		}
+
+		return nil
+		},
+	}
+
+
+
+func init() {
+
+	namespaceForceDelete.Flags().DurationVar(&timeout, "timeout",time.Minute * 2, "specify the timeout")
+
+	Namespace.AddCommand(namespaceForceDelete)
+}
+
