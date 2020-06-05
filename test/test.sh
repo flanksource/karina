02fe0689207276bdf306a80905955e826106bf0b
Moshe Immerman <moshe.immerman@gmail.com>
2020-06-05 08:26:10 +0200

    fix gh actions

--- test/test.sh
+++ test/test.sh
@@ -43,7 +43,7 @@ if ! $BIN test all --e2e --progress=false -v --junit-path test-results/results.x
   failed=true
 fi
 
-wget -nv https://github.com/flanksource/build-tools/releases/download/v0.8.0/build-tools
+wget -nv https://github.com/flanksource/build-tools/releases/download/v0.9.4/build-tools
 chmod +x build-tools
 ./build-tools gh actions report-junit test-results/results.xml --token $GIT_API_KEY
 
