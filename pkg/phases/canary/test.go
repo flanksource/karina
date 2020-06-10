3e8e00e6a8e1938b3c83584cb9c3ca9f44f6b60f
Philip Stafford Wood <philip.wood@gmail.com>
2020-06-10 14:41:43 +0200

    linter fixes

--- pkg/phases/canary/test.go
+++ pkg/phases/canary/test.go
@@ -2,10 +2,11 @@ package canary
 
 import (
 	"crypto/tls"
+	"net/http"
+
 	"github.com/flanksource/commons/console"
 	"github.com/flanksource/karina/pkg/platform"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"net/http"
 )
 
 const testName = "canary"
@@ -17,29 +18,28 @@ func TestCanary(p *platform.Platform, test *console.TestResults) {
 	}
 	client, err := p.GetClientset()
 	if err != nil {
-		test.Failf(testName,"couldn't get clientset: %v", err)
+		test.Failf(testName, "couldn't get clientset: %v", err)
 		return
 	}
 
-	ingress, err := client.ExtensionsV1beta1().Ingresses("monitoring").Get("canary",v1.GetOptions{})
+	ingress, err := client.ExtensionsV1beta1().Ingresses("monitoring").Get("canary", v1.GetOptions{})
 	if err != nil {
-		test.Failf(testName,"couldn't get ingress: %v", err)
+		test.Failf(testName, "couldn't get ingress: %v", err)
 		return
 	}
 	host := ingress.Spec.Rules[0].Host
 
-	url := "http://"+host+"/metrics"
+	url := "http://" + host + "/metrics"
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	resp, err := http.Get(url)
 	if err != nil {
-		test.Failf(testName,"couldn't GET %v : %v", url, err)
+		test.Failf(testName, "couldn't GET %v : %v", url, err)
 		return
 	}
 	defer resp.Body.Close()
 	if resp.Status != "200 OK" {
-		test.Failf(testName,"metric status was not OK : %v", resp.Status)
+		test.Failf(testName, "metric status was not OK : %v", resp.Status)
 		return
 	}
 	test.Passf(testName, "canary-checker metrics page up")
-
 }
