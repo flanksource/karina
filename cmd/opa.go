9c3ea4bf7fa2a597c8a1618e4d11031862eedc44
Moshe Immerman <moshe.immerman@gmail.com>
2020-06-11 20:23:32 +0200

    add additional validations

--- cmd/opa.go
+++ cmd/opa.go
@@ -14,7 +14,7 @@ func init() {
 	Opa.AddCommand(&cobra.Command{
 		Use:   "deploy-bundle",
 		Short: "deploy opa bundle",
-		Args:  cobra.MinimumNArgs(0),
+		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			if err := opa.DeployBundle(getPlatform(cmd), args[0]); err != nil {
 				log.Fatalf("Error deploying  opa bundles: %s", err)
