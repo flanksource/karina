6e4b546047b1ca2d1e3ec5e51649375e1574d891
Philip Stafford Wood <philip.wood@gmail.com>
2020-07-09 09:01:56 +0200

    workaround for 'sops exec-env' not returning status codes

--- test/vsphere/e2e.sh
+++ test/vsphere/e2e.sh
@@ -60,8 +60,11 @@ zip -r artifacts/snapshot.zip snapshot/*
 $BIN terminate-orphans $PLATFORM_OPTIONS_FLAGS || echo "Orphans not terminated."
 $BIN cleanup $PLATFORM_OPTIONS_FLAGS
 
-build-tools junit passfail test-results/results.xml || exit 1
+# serves as a workaround for `sops exec-env` not passing exit codes
+# in current release: https://github.com/mozilla/sops/issues/626
+build-tools junit passfail test-results/results.xml || echo "TEST_FAILURE"
 
 if [[ "$failed" == true ]]; then
+  echo "TEST_FAILURE"
   exit 1
 fi
