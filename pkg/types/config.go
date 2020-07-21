6eed66e2e0ec23d1529cac477e6d34ad74c01a5d
Moshe Immerman <moshe.immerman@gmail.com>
2020-07-21 13:25:01 +0200

    fix kind type

--- pkg/types/config.go
+++ pkg/types/config.go
@@ -25,6 +25,7 @@ type PlatformConfig struct {
 	ImportConfigs         []string            `yaml:"importConfigs,omitempty"`
 	IngressCA             *CA                 `yaml:"ingressCA"`
 	GitOps                []GitOps            `yaml:"gitops,omitempty"`
+	Kind                  Kind                `yaml:"kind,omitempty"`
 	Kiosk                 Kiosk               `yaml:"kiosk,omitempty"`
 	KubeWebView           *KubeWebView        `yaml:"kubeWebView,omitempty"`
 	KubeResourceReport    *KubeResourceReport `yaml:"kubeResourceReport,omitempty"`
