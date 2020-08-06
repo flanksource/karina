dfd3e48167935a634472f720d706a9014d3e510f
Narayana108 <narayan-das@tuta.io>
2020-08-06 13:17:51 +0200

    delete-pods

--- cmd/delete-all-pods.go
+++ cmd/delete-all-pods.go
@@ -7,10 +7,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// Potentially deploys pods to new node
+// The deletion of pods gives the potentiality to deploys pods to new nodes
 var DeleteAllPods = &cobra.Command{
 	Use:   "delete-all-pods",
-	Short: "Delete All Pods",
+	Short: "Delete all pods",
 	Args:  cobra.MinimumNArgs(0),
   Run: func(cmd *cobra.Command, args []string) {
 
@@ -20,7 +20,7 @@ var DeleteAllPods = &cobra.Command{
 			log.Fatalf("unable to get clientset: %v", err)
 		}
 
-    // Get all pods information in all namespaces
+    // Get all pods information from all namespaces
     pods, err := client.CoreV1().Pods("").List(metav1.ListOptions{})
 
     if err != nil {
@@ -32,9 +32,9 @@ var DeleteAllPods = &cobra.Command{
         // List Pods
         fmt.Printf("Deleting pod: %s\n", pod.GetName())
         // Delete Pods
-        //if err = client.CoreV1().Pods(pod.GetNamespace()).Delete(pod.GetName(), nil); err != nil {
-          //p.Errorf("failed to delete pod %s", pod.GetName())
-        //}
+        if err = client.CoreV1().Pods(pod.GetNamespace()).Delete(pod.GetName(), nil); err != nil {
+          p.Errorf("failed to delete pod %s", pod.GetName())
+        }
       }
     } else {
       fmt.Println("No pods found!")
