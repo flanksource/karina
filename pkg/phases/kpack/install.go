4b9608b32153a9fb842ed873945a2ad7efe15f47
Moshe Immerman <moshe.immerman@gmail.com>
2020-09-29 20:40:39 +0200

    add kpack

---
+++ pkg/phases/kpack/install.go
@@ -0,0 +1,21 @@
+package kpack
+
+import (
+	"github.com/flanksource/karina/pkg/platform"
+)
+
+const (
+	Namespace = "kpack"
+)
+
+func Install(p *platform.Platform) error {
+	if p.Kpack.IsDisabled() {
+		return p.DeleteSpecs(Namespace, "kpack.yaml")
+	}
+
+	if err := p.CreateOrUpdateNamespace(Namespace, nil, nil); err != nil {
+		return err
+	}
+
+	return p.ApplySpecs(Namespace, "kpack.yaml")
+}
