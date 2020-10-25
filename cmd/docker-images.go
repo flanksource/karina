2f3736680f1a0acc4425451ebf30c9ff7203213d
Moshe Immerman <moshe.immerman@gmail.com>
2020-10-25 14:10:43 +0200

    add images sync

--- cmd/docker-images.go
+++ cmd/docker-images.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"strings"
 
-	log "github.com/sirupsen/logrus"
+	"github.com/flanksource/commons/exec"
+	"github.com/flanksource/karina/pkg/platform"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"gopkg.in/flanksource/yaml.v3"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
@@ -15,46 +17,60 @@ var Images = &cobra.Command{
 	Short: "Commands for working with docker images",
 }
 
+func getImages(p *platform.Platform) ([]string, error) {
+	images := []string{}
+	// in order to list all images we perform an dry-run deployment
+	// with an ApplyHook
+	p.DryRun = true
+	p.ApplyDryRun = true
+	p.TerminationProtection = true
+	p.ApplyHook = func(ns string, obj unstructured.Unstructured) {
+		containers := []interface{}{}
+		if image, found := obj.GetAnnotations()["image"]; found {
+			images = append(images, image)
+		}
+		list, found, _ := unstructured.NestedSlice(obj.UnstructuredContent(), "spec", "template", "spec", "containers")
+		if found {
+			containers = append(containers, list...)
+		}
+		list, found, _ = unstructured.NestedSlice(obj.UnstructuredContent(), "spec", "template", "spec", "initContainers")
+		if found {
+			containers = append(containers, list...)
+		}
+		for _, container := range containers {
+			image, found := container.(map[string]interface{})["image"].(string)
+			if found {
+				images = append(images, image)
+			}
+		}
+	}
+
+	for name, fn := range Phases {
+		if err := fn(p); err != nil {
+			return nil, errors.Wrapf(err, "error deploying %s", name)
+		}
+	}
+	return images, nil
+}
+
 func init() {
 	listCmd := &cobra.Command{
 		Use:   "list",
 		Short: "List all docker images used by the platform",
 		Args:  cobra.MinimumNArgs(0),
 		Run: func(cmd *cobra.Command, args []string) {
-			images := []string{}
-
 			outputFormat, _ := cmd.Flags().GetString("output")
-
 			p := getPlatform(cmd)
-			// in order to list all images we perform an dry-run deployment
-			// with an ApplyHook
-			p.DryRun = true
-			p.ApplyDryRun = true
-			p.TerminationProtection = true
-			p.ApplyHook = func(ns string, obj unstructured.Unstructured) {
-				containers := []interface{}{}
-				if image, found := obj.GetAnnotations()["image"]; found {
-					images = append(images, image)
-				}
-				list, found, _ := unstructured.NestedSlice(obj.UnstructuredContent(), "spec", "template", "spec", "containers")
-				if found {
-					containers = append(containers, list...)
-				}
-				list, found, _ = unstructured.NestedSlice(obj.UnstructuredContent(), "spec", "template", "spec", "initContainers")
-				if found {
-					containers = append(containers, list...)
-				}
-				for _, container := range containers {
-					image, found := container.(map[string]interface{})["image"].(string)
-					if found {
-						images = append(images, image)
-					}
-				}
+			images, err := getImages(p)
+			if err != nil {
+				p.Fatalf("Failed to dry-run deploy : %v", err)
 			}
-
-			for name, fn := range Phases {
-				if err := fn(p); err != nil {
-					log.Errorf("Failed to dry-run deploy %s: %v", name, err)
+			if p.DockerRegistry != "" {
+				l := len(images)
+				for i := 0; i < l; i++ {
+					if !strings.HasPrefix(images[i], p.DockerRegistry) {
+						images[i] = p.DockerRegistry + "/" + images[i]
+					}
 				}
 			}
 
@@ -74,6 +90,18 @@ func init() {
 		Use:   "sync",
 		Short: "Synchronize all platform docker images to a local registry",
 		Args:  cobra.MinimumNArgs(0),
-		Run:   func(cmd *cobra.Command, args []string) {},
+		Run: func(cmd *cobra.Command, args []string) {
+			p := getPlatform(cmd)
+			images, err := getImages(p)
+			if err != nil {
+				p.Fatalf("Failed to dry-run deploy : %v", err)
+			}
+			for _, image := range images {
+				p.Infof("Syncing %s", image)
+				_ = exec.Execf("docker pull %s", image)
+				_ = exec.Execf("docker tag %s %s/%s", image, p.DockerRegistry, image)
+				_ = exec.Execf("docker push %s/%s", p.DockerRegistry, image)
+			}
+		},
 	})
 }
