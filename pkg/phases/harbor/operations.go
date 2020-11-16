575010d0209f910278a9515ecff8a2609a25dcac
Teodor <teodor.pripoae@gmail.com>
2020-11-16 19:41:07 +0200

    Add command to list harbor projects, images and tags

--- pkg/phases/harbor/operations.go
+++ pkg/phases/harbor/operations.go
@@ -1,10 +1,15 @@
 package harbor
 
 import (
+	"context"
 	"fmt"
+	"sort"
+	"strings"
+	"sync"
 
 	"github.com/flanksource/karina/pkg/platform"
 	"github.com/pkg/errors"
+	"golang.org/x/sync/semaphore"
 )
 
 func ReplicateAll(p *platform.Platform) error {
@@ -39,20 +44,138 @@ func UpdateSettings(p *platform.Platform) error {
 	return client.UpdateSettings(*p.Harbor.Settings)
 }
 
-func ListImages(p *platform.Platform, project string, listTags bool) error {
+func ListProjects(p *platform.Platform) ([]Project, error) {
 	client, err := NewClient(p)
 	if err != nil {
-		return errors.Wrap(err, "failed to create harbor client")
+		return nil, errors.Wrap(err, "failed to create harbor client")
 	}
 
-	images, err := client.ListImages(project)
+	projects, err := client.ListProjects()
 	if err != nil {
-		return errors.Wrapf(err, "failed to list images for project %s", project)
+		return nil, errors.Wrapf(err, "failed to list projects")
 	}
 
-	for _, image := range images {
-		fmt.Printf("Name: %s\n", image.Name)
+	return projects, nil
+}
+
+func ListImagesWithTags(p *platform.Platform, concurrency int) ([]Tag, error) {
+	client, err := NewClient(p)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create harbor client")
 	}
 
-	return nil
+	images, err := ListImages(p, concurrency)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to list images")
+	}
+
+	lock := semaphore.NewWeighted(int64(concurrency))
+
+	var wg sync.WaitGroup
+	wg.Add(len(images))
+
+	allTags := []Tag{}
+	mtx := &sync.Mutex{}
+
+	for _, i := range images {
+		p.Debugf("artifacts count for %s/%s: %d\n", i.ProjectName, i.Name, i.ArtifactCount)
+
+		go func(project, image string) {
+			lock.Acquire(context.Background(), 1)
+			defer func() {
+				lock.Release(1)
+				wg.Done()
+			}()
+			tags, err := client.ListTags(project, image)
+			if err != nil {
+				p.Errorf("failed to list tags for image %s in project %s: %v", image, project, err)
+			}
+			p.Tracef("tags count for %s/%s: %d\n", project, image, len(tags))
+
+			for i := range tags {
+				tags[i].ProjectName = project
+				tags[i].RepositoryName = image
+
+				if strings.HasPrefix(tags[i].Digest, "sha256:") {
+					tags[i].Digest = strings.TrimPrefix(tags[i].Digest, "sha256:")
+				}
+			}
+
+			mtx.Lock()
+			allTags = append(allTags, tags...)
+			mtx.Unlock()
+		}(i.ProjectName, i.Name)
+	}
+
+	wg.Wait()
+
+	sort.SliceStable(allTags, func(i, j int) bool {
+		if allTags[i].ProjectName == allTags[j].ProjectName {
+			if allTags[i].RepositoryName == allTags[j].RepositoryName {
+				return allTags[i].Digest < allTags[j].Digest
+			}
+			return allTags[i].RepositoryName < allTags[j].RepositoryName
+		}
+		return allTags[i].ProjectName < allTags[j].ProjectName
+	})
+
+	return allTags, nil
+}
+
+func ListImages(p *platform.Platform, concurrency int) ([]Image, error) {
+	client, err := NewClient(p)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create harbor client")
+	}
+
+	projects, err := client.ListProjects()
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to list projects")
+	}
+
+	lock := semaphore.NewWeighted(int64(concurrency))
+
+	var wg sync.WaitGroup
+	wg.Add(len(projects))
+
+	allImages := []Image{}
+	mtx := &sync.Mutex{}
+
+	for _, project := range projects {
+		p.Debugf("listing images for project %s", project.Name)
+
+		go func(projectName string) {
+			lock.Acquire(context.Background(), 1)
+			defer func() {
+				lock.Release(1)
+				wg.Done()
+			}()
+			images, err := client.ListImages(projectName)
+			if err != nil {
+				p.Errorf("failed to list images in project %s: %v", project, err)
+			}
+
+			for i := range images {
+				images[i].ProjectName = projectName
+
+				parts := strings.SplitN(images[i].Name, "/", 2)
+				images[i].Name = parts[1]
+			}
+
+			mtx.Lock()
+			allImages = append(allImages, images...)
+			mtx.Unlock()
+		}(project.Name)
+	}
+
+	wg.Wait()
+
+	sort.SliceStable(allImages, func(i, j int) bool {
+		if allImages[i].ProjectName == allImages[j].ProjectName {
+			return allImages[i].Name < allImages[j].Name
+		}
+		return allImages[i].ProjectName < allImages[j].ProjectName
+	})
+
+	return allImages, nil
 }
