156a219dcd4789483789f788b3e8fdafc2c854e4
Teodor Pripoae <teodor.pripoae@gmail.com>
2020-11-24 19:25:20 +0200

    Fix opa install index out of range

--- pkg/phases/opa/install.go
+++ pkg/phases/opa/install.go
@@ -150,7 +150,7 @@ func InstallGatekeeper(p *platform.Platform) error {
 	clientset, _ := p.GetClientset()
 	for {
 		webhook, _ := clientset.AdmissionregistrationV1beta1().MutatingWebhookConfigurations().Get(context.TODO(), "gatekeeper-validating-webhook-configuration", metav1.GetOptions{})
-		if webhook != nil && len(webhook.Webhooks[0].ClientConfig.CABundle) > 100 {
+		if webhook != nil && len(webhook.Webhooks) > 0 && len(webhook.Webhooks[0].ClientConfig.CABundle) > 100 {
 			break
 		} else {
 			time.Sleep(3 * time.Second)
