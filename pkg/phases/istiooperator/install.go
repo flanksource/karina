eeceb50cf4fbb454e374b9d73555cf86b07a5ae9
Kyle Robbertze <kyle@paddatrapper.com>
2021-02-01 17:05:54 +0200

    temporary debugging

--- pkg/phases/istiooperator/install.go
+++ pkg/phases/istiooperator/install.go
@@ -10,8 +10,10 @@ const (
 
 func Install(p *platform.Platform) error {
 	if p.IstioOperator.IsDisabled() {
+		p.Logger.Errorf("DELETING because Istio operator disabled")
 		return p.DeleteSpecs(Namespace, "istio-operator.yaml")
 	}
 
+	p.Logger.Errorf("APPLYING because Istio operator enabled")
 	return p.ApplySpecs(Namespace, "istio-operator.yaml")
 }
