916b3e4f4c7425f19c0940c4498f82ab43cfb297
Moshe Immerman <moshe@flanksource.com>
2021-03-02 13:46:01 +0200

    chore: move namespaceConfigurator to template-operator

--- pkg/phases/templateoperator/test.go
+++ pkg/phases/templateoperator/test.go
@@ -15,9 +15,28 @@ func Test(p *platform.Platform, test *console.TestResults) {
 
 	client, err := p.GetClientset()
 	if err != nil {
-		test.Failf(Namespace, "Could not connect to Platform client: %v", err)
+		test.Failf(Name, "Could not connect to Platform client: %v", err)
 		return
 	}
-	p.WaitForNamespace(Namespace, 60*time.Second)
-	kommons.TestNamespace(client, Namespace, test)
+	if err := p.WaitForDeployment(Namespace, Name, 60*time.Second); err != nil {
+		test.Failf(Name, "template-operator did not become ready")
+		return
+	}
+	kommons.TestDeploy(client, Namespace, Name, test)
+	TestKommonsTemplate(p, test)
+}
+
+func TestKommonsTemplate(platform *platform.Platform, test *console.TestResults) {
+	testName := "kommons-template"
+
+	templateResult, err := platform.TemplateText(`{{  kget "cm/quack/quack-config" "data.domain"  }}`)
+	if err != nil {
+		test.Failf(testName, "failed to template: %v", err)
+		return
+	}
+	if templateResult != platform.Domain {
+		test.Failf(testName, "expected templated value to equal %s, got %s", platform.Domain, templateResult)
+	} else {
+		test.Passf(testName, "kget pulled cm/quack/quack-config successfully")
+	}
 }
