52ac3ba3e789c24a051a4b86bcc8343b43e29805
Long Nguyen <long.nguyen@logrhythm.com>
2021-03-02 22:48:58 -0700

    fix(keptn): fix Keptn test phase

--- pkg/phases/keptn/test.go
+++ pkg/phases/keptn/test.go
@@ -12,6 +12,7 @@ func Test(p *platform.Platform, test *console.TestResults) {
 		test.Skipf("keptn", "Keptn is disabled")
 		return
 	}
+	testName := "Keptn"
 	expectedKeptnDeployments := []string{
 		"api-gateway-nginx",
 		"api-service",
@@ -20,16 +21,26 @@ func Test(p *platform.Platform, test *console.TestResults) {
 		"eventbroker-go",
 		"lighthouse-service",
 		"mongodb-datastore",
-		"mongodb-keptn",
 		"remediation-service",
 		"shipyard-service",
 	}
 	for _, deployName := range expectedKeptnDeployments {
 		err := p.WaitForDeployment(Namespace, deployName, 1*time.Minute)
 		if err != nil {
-			test.Failf("Keptn", "Keptn component %s is not healthy: %v", deployName, err)
+			test.Failf(testName, "Keptn component %s (Deployment) is not healthy: %v", deployName, err)
 			return
 		}
 	}
-	test.Passf("Keptn", "Keptn is healthy")
+	expectedKeptnStatefulsets := []string{
+		"keptn-nats-cluster",
+		"mongodb-keptn",
+	}
+	for _, statefulsetName := range expectedKeptnStatefulsets {
+		err := p.WaitForStatefulSet(Namespace, statefulsetName, 1*time.Minute)
+		if err != nil {
+			test.Failf(testName, "Keptn component %s (StatefulSet) is not healthy: %v", statefulsetName, err)
+			return
+		}
+	}
+	test.Passf(testName, "Keptn is healthy")
 }
