18c15d39d1df8bbf1b11ba7b237565bb3dde712a
Moshe Immerman <moshe@flanksource.com>
2021-03-05 18:03:02 +0200

    chore: fix antrea deployment

--- pkg/phases/antrea/install.go
+++ pkg/phases/antrea/install.go
@@ -1,14 +1,15 @@
 package antrea
 
 import (
-	"github.com/flanksource/karina/pkg/platform"
 	"time"
+
+	"github.com/flanksource/karina/pkg/platform"
 )
 
 const (
 	Namespace  = "kube-system"
 	Controller = "antrea-controller"
-	DaemonSet  = "anrtea-agent"
+	DaemonSet  = "antrea-agent"
 )
 
 func Install(p *platform.Platform) error {
@@ -24,8 +25,8 @@ func Install(p *platform.Platform) error {
 		return err
 	}
 
-	if err := p.WaitForDeployment(Namespace, Controller, 2*time.Minute); err != nil {
+	if err := p.WaitForDeployment(Namespace, Controller, 10*time.Minute); err != nil {
 		return err
 	}
-	return p.WaitForDaemonSet(Namespace, DaemonSet, 2*time.Minute)
+	return p.WaitForDaemonSet(Namespace, DaemonSet, 10*time.Minute)
 }
