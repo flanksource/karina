d11db521f779f36d5266b9fa6e72c7e66c80ec58
Moshe Immerman <moshe@flanksource.com>
2021-03-05 14:40:54 +0200

    fix: wait for CNI readiness before deploying

--- pkg/phases/antrea/install.go
+++ pkg/phases/antrea/install.go
@@ -4,7 +4,11 @@ import (
 	"github.com/flanksource/karina/pkg/platform"
 )
 
-const Namespace = "kube-system"
+const (
+	Namespace  = "kube-system"
+	Controller = "antrea-controller"
+	DaemonSet  = "anrtea-agent"
+)
 
 func Install(p *platform.Platform) error {
 	if !p.Calico.IsDisabled() && !p.Antrea.IsDisabled() {
@@ -15,5 +19,12 @@ func Install(p *platform.Platform) error {
 		return p.DeleteSpecs(Namespace, "antrea.yaml")
 	}
 	p.Antrea.IsCertReady = false
-	return p.ApplySpecs(Namespace, "antrea.yaml")
+	if err := p.ApplySpecs(Namespace, "antrea.yaml"); err != nil {
+		return err
+	}
+
+	if err := platform.WaitForDeployment(Namespace, Controller, 2*time.Minute); err != nil {
+		return err
+	}
+	return platform.WaitForDaemonSet(Namespace, DaemonSet, 2*time.Minute)
 }
