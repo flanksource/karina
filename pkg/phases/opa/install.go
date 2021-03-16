fb1341fcf07108d9dffa195a4c0e0c1b3fbc129f
Moshe Immerman <moshe@flanksource.com>
2021-03-16 19:55:19 +0200

    chore: improve the reliability opa deploy

--- pkg/phases/opa/install.go
+++ pkg/phases/opa/install.go
@@ -1,15 +1,8 @@
 package opa
 
 import (
-	"context"
-	"fmt"
-	"strings"
-	"time"
-
 	"github.com/flanksource/karina/pkg/constants"
 	"github.com/flanksource/karina/pkg/platform"
-	"github.com/pkg/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -25,6 +18,12 @@ func Install(p *platform.Platform) error {
 		return p.DeleteSpecs(Namespace, "opa-gatekeeper.yaml")
 	}
 
+	if p.DryRun && !p.Gatekeeper.IsDisabled() {
+		_ = p.ApplySpecs(Namespace, "opa-gatekeeper.yaml", "opa-gatekeeper-monitoring.yaml.raw")
+	} else if p.DryRun {
+		return nil
+	}
+
 	if err := p.CreateOrUpdateNamespace(Namespace, nil, nil); err != nil {
 		return err
 	}
@@ -36,10 +35,6 @@ func Install(p *platform.Platform) error {
 
 	if err := p.ApplySpecs(Namespace, "opa-gatekeeper.yaml", "opa-gatekeeper-monitoring.yaml.raw"); err != nil {
 		return err
-	}
-
-	if p.DryRun {
-		return nil
 	}
 
 	webhooks, err := p.CreateWebhookBuilder(Namespace, WebhookService, ca)
@@ -70,10 +65,6 @@ func Install(p *platform.Platform) error {
 		return err
 	}
 
-	if err := waitForTemplates(p); err != nil {
-		return err
-	}
-
 	if err := deployConstraints(p, p.Gatekeeper.Constraints); err != nil {
 		return err
 	}
@@ -96,63 +87,5 @@ func deployDefaultTemplates(p *platform.Platform) error {
 		}
 	}
 
-	return nil
-}
-
-func waitForTemplates(p *platform.Platform) error {
-	start := time.Now()
-	k8sclient, err := p.GetClientset()
-	if err != nil {
-		return errors.Wrap(err, "Failed to create clientset")
-	}
-
-	templateClient, err := p.GetClientByKind("ConstraintTemplate")
-	if err != nil {
-		return err
-	}
-
-	for {
-		templateList, err := templateClient.List(context.TODO(), metav1.ListOptions{})
-
-		if err != nil {
-			return err
-		}
-
-		if start.Add(60 * time.Second).Before(time.Now()) {
-			return fmt.Errorf("timeout exceeded waiting for ConstraintTemplates")
-		}
-
-		ready := true
-		for _, template := range templateList.Items {
-			ctName := template.Object["metadata"].(map[string]interface{})["name"].(string)
-			p.Debugf("Checking creation status of %s", ctName)
-			if template.Object["status"] != nil && !template.Object["status"].(map[string]interface{})["created"].(bool) {
-				ready = false
-			}
-		}
-
-		_, resources, err := k8sclient.ServerGroupsAndResources()
-		if err != nil {
-			return errors.Wrap(err, "Failed to obtain server resources")
-		}
-
-		var constraintTemplates []metav1.APIResource
-		for _, res := range resources {
-			if strings.HasPrefix(res.GroupVersion, "constraints.gatekeeper.sh") {
-				constraintTemplates = res.APIResources
-				break
-			}
-		}
-		p.Debugf("Checking creation of constraint resources")
-		if len(constraintTemplates) < len(templateList.Items) {
-			ready = false
-		}
-
-		if ready {
-			break
-		}
-
-		time.Sleep(1 * time.Second)
-	}
 	return nil
 }
